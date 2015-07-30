9747b06e00f112d8d273625f130438c75c9ca181
clearostranslations <translations@clearfoundation.com>
2015-07-29 21:56:44 -0400

    Updated translations

--- language/fa_IR/dropbox_lang.php
+++ language/fa_IR/dropbox_lang.php
@@ -12,6 +12,7 @@ $lang['dropbox_reset_complete'] = 'Your local Dropbox local folder has been dele
 $lang['dropbox_reset_confirm'] = 'It is recommended you disassociate this device from the Dropbox portal before resetting (deleting) your local Dropbox folder.';
 $lang['dropbox_reset_delete'] = 'Reset/Delete Local Folder';
 $lang['dropbox_service_not_running'] = 'Dropbox service is not running.';
+$lang['dropbox_starting_service'] = 'You will not be able to start the Dropbox service until at least one user has configured their account.  Login to Webconfig as a user (non-root) and run through the Dropbox syncronization setup.';
 $lang['dropbox_status_not_initialized'] = 'Pending user initialization';
 $lang['dropbox_user_invalid'] = 'Dropbox user is invalid.';
 $lang['dropbox_user_no_access'] = 'The administrator has not set up this account with Dropbox access.';
