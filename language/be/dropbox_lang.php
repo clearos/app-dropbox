8aa0ecf072d445616ffb362f682968fc701de913
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:06:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox служба занятая ініцыялізацыі іншага карыстальніка.';
+$lang['dropbox_app_description'] = 'Dropbox гэта сэрвіс файлавага хостынгу кіруецца Dropbox, Inc., якая прапануе воблака захоўвання, сінхранізацыі файлаў і кліенцкае праграмнае забеспячэнне.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'памер папкі';
+$lang['dropbox_missing_home_folder'] = 'Адсутнічае хатняя тэчка - праверка доступу карыстальнікаў да службам файлаў.';
+$lang['dropbox_no_data'] = 'Дадзеных няма ...';
+$lang['dropbox_reset_complete'] = 'Ваша мясцовая лакальная тэчка Dropbox быў выдалены і кошт сінхранізацыі скіду інфармацыі.';
+$lang['dropbox_reset_confirm'] = 'Рэкамендуецца, каб Вы раз&#39;яднаць гэта прылада з партала Dropbox перад скідам (выдаленне) лакальнай тэчцы Dropbox.';
+$lang['dropbox_reset_delete'] = 'Скід / Выдаліць лакальную тэчку';
+$lang['dropbox_service_not_running'] = 'Dropbox служба ня запушчана.';
+$lang['dropbox_starting_service'] = 'Вы не зможаце запусціць службу Dropbox, пакуль па крайняй меры адзін карыстальнік не наладзіў свой рахунак. Уваход у WebConfig ў якасці карыстальніка (некорневой) і запусціць праз ўстаноўку Syncronization Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'Да ініцыялізацыі карыстальніка';
+$lang['dropbox_user_invalid'] = 'карыстальнік Dropbox з&#39;яўляецца несапраўдным.';
+$lang['dropbox_user_no_access'] = 'Адміністратар не ўсталяваў гэты конт з доступам у Dropbox.';
+$lang['dropbox_users'] = 'Dropbox Карыстальнікі';
