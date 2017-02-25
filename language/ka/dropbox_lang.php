320e7729405aba4a917a7856643cf344bb5ae838
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:10:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox მომსახურება დაკავებულია ინიციალიზაციისას სხვა მომხმარებლის.';
+$lang['dropbox_app_description'] = 'Dropbox არის ჰოსტინგი ექსპლუატაციას Dropbox, Inc., რომელიც გთავაზობთ ღრუბელი შენახვის, ფაილი სინქრონიზაცია და კლიენტის პროგრამული უზრუნველყოფა.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'ფოლდერის ზომა';
+$lang['dropbox_missing_home_folder'] = 'დაკარგული მთავარი ფოლდერი - შემოწმება მომხმარებლის ხელმისაწვდომობის ფაილი მომსახურება.';
+$lang['dropbox_no_data'] = 'მონაცემები არ არის...';
+$lang['dropbox_reset_complete'] = 'თქვენი ადგილობრივი Dropbox ადგილობრივ ფოლდერი წაიშალა და ანგარიშის sync ინფორმაცია გადატვირთვა.';
+$lang['dropbox_reset_confirm'] = 'მიზანშეწონილია თქვენ თქვას ამ მოწყობილობის Dropbox ვერსია ადრე გადატვირთვის (წაშლის) თქვენს Dropbox საქაღალდე.';
+$lang['dropbox_reset_delete'] = 'აღდგენა / წაშლა ადგილობრივი Folder';
+$lang['dropbox_service_not_running'] = 'Dropbox მომსახურება არ არის გაშვებული.';
+$lang['dropbox_starting_service'] = 'თქვენ ვერ შეძლებთ დაიწყოს Dropbox წლამდე მინიმუმ ერთი მომხმარებელმა დაუყენებია ანგარიშზე. შესვლა Webconfig როგორც მომხმარებელი (არასამთავრობო root) და აწარმოებს მეშვეობით Dropbox syncronization setup.';
+$lang['dropbox_status_not_initialized'] = 'მოლოდინში შესახებ ინიციალიზაციისას';
+$lang['dropbox_user_invalid'] = 'Dropbox შესახებ არასწორია.';
+$lang['dropbox_user_no_access'] = 'ადმინისტრატორმა არ შეიქმნა ეს ანგარიში Dropbox ხელმისაწვდომობა.';
+$lang['dropbox_users'] = 'Dropbox მომხმარებელი';
