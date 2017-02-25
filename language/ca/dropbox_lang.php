52581a3c20003a24b16bf2133af07cec82aea6a5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:07:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'servei de Dropbox està ocupat inicialitzar un altre usuari.';
+$lang['dropbox_app_description'] = 'Dropbox és un servei d&#39;allotjament d&#39;arxius operat per Dropbox, Inc. que ofereix emmagatzematge en el núvol, sincronització d&#39;arxius i programari de client.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'mida de la carpeta';
+$lang['dropbox_missing_home_folder'] = 'Missing carpeta d&#39;inici - comprovar el vostre accés als serveis d&#39;arxiu.';
+$lang['dropbox_no_data'] = 'No hi ha dades...';
+$lang['dropbox_reset_complete'] = 'La seva carpeta local local de Dropbox ha estat esborrada i sincronització de comptes de reposició informació.';
+$lang['dropbox_reset_confirm'] = 'Es recomana que desassociar aquest dispositiu des del portal de Dropbox abans de restablir la carpeta de Dropbox local (esborrar).';
+$lang['dropbox_reset_delete'] = 'Restablir / Esborrar Carpeta local';
+$lang['dropbox_service_not_running'] = 'Dropbox servei no s&#39;està executant.';
+$lang['dropbox_starting_service'] = 'Vostè no serà capaç d&#39;iniciar el servei Dropbox fins a almenys un usuari ha configurat el seu compte. Inicia sessió per WEBconfig com a usuari (no root) i s&#39;executi a través de la configuració de sincronització de Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'A l&#39;espera d&#39;inicialització de l&#39;usuari';
+$lang['dropbox_user_invalid'] = 'Dropbox usuari no és vàlid.';
+$lang['dropbox_user_no_access'] = 'L&#39;administrador no ha establert aquest compte amb accés a Dropbox.';
+$lang['dropbox_users'] = 'Els usuaris de Dropbox';
