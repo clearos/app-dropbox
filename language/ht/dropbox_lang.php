24bb8c339e312895373ee8c5a2f781867a0e8b1a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:09:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'sèvis Dropbox se okipe inisyalizin yon lòt itilizatè.';
+$lang['dropbox_app_description'] = 'Dropbox se yon sèvis dosye hosting opere pa Dropbox, Inc. ki ofri depo nwaj, senkronizasyon dosye ak lojisyèl kliyan.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'dosye Kantite moun ki';
+$lang['dropbox_missing_home_folder'] = 'Ki manke katab kay - tcheke aksè itilizatè a sèvis dosye-a.';
+$lang['dropbox_no_data'] = 'Pa gen done ...';
+$lang['dropbox_reset_complete'] = 'te lokal katab Dropbox ou lokal te efase ak kont senk Reyajiste enfòmasyon.';
+$lang['dropbox_reset_confirm'] = 'Li rekòmande ou dissocier aparèy sa a soti nan pòtal la Dropbox anvan re (efase) katab Dropbox lokal ou a.';
+$lang['dropbox_reset_delete'] = 'Reyajiste / Efase Dosye Lokal';
+$lang['dropbox_service_not_running'] = 'sèvis Dropbox se pa kouri.';
+$lang['dropbox_starting_service'] = 'Ou pa yo pral kapab kòmanse sèvis nan Dropbox jiskaske omwen yon itilizatè configuré kont yo. Login to Webconfig kòm yon itilizatè (ki pa rasin) ak kouri nan konfigirasyon an syncronization Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'Annatandan itilizatè inisyalizasyon';
+$lang['dropbox_user_invalid'] = 'itilizatè Dropbox se valab.';
+$lang['dropbox_user_no_access'] = 'Administratè a pa te mete kanpe kont sa a ak Dropbox aksè.';
+$lang['dropbox_users'] = 'Itilizatè yo Dropbox';
