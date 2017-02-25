4ff3cfc6868cba9a7255226798b89d1bde3b95fa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:16:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'บริการ Dropbox ไม่ว่างเริ่มต้นผู้ใช้คนอื่น';
+$lang['dropbox_app_description'] = 'Dropbox เป็นไฟล์ที่บริการพื้นที่ดำเนินการโดย Dropbox, Inc ที่มีการจัดเก็บเมฆประสานไฟล์และลูกค้าซอฟแวร์';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'ขนาดโฟลเดอร์';
+$lang['dropbox_missing_home_folder'] = 'หายไปโฟลเดอร์บ้าน - ตรวจสอบที่ผู้ใช้สามารถเข้าถึงบริการไฟล์';
+$lang['dropbox_no_data'] = 'ไม่มีข้อมูล...';
+$lang['dropbox_reset_complete'] = 'โฟลเดอร์ Dropbox ท้องถิ่นท้องถิ่นของคุณได้รับการลบและการตั้งค่าบัญชีข้อมูลการซิงค์';
+$lang['dropbox_reset_confirm'] = 'จึงแนะนำให้คุณยกเลิกการเชื่อมโยงอุปกรณ์นี้ออกจากพอร์ทัล Dropbox ก่อนรีเซ็ต (ลบ) โฟลเดอร์ Dropbox ท้องถิ่นของคุณ';
+$lang['dropbox_reset_delete'] = 'รีเซ็ต / ลบโฟลเดอร์ภายใน';
+$lang['dropbox_service_not_running'] = 'บริการ Dropbox ไม่ได้ทำงาน';
+$lang['dropbox_starting_service'] = 'คุณจะไม่สามารถที่จะเริ่มบริการ Dropbox จนกว่าผู้ใช้อย่างน้อยหนึ่งมีการกำหนดค่าบัญชีของพวกเขา เข้าสู่ระบบเพื่อ webconfig เป็นผู้ใช้ (ที่ไม่ใช่ราก) และเรียกใช้ผ่านการตั้งค่า syncronization Dropbox';
+$lang['dropbox_status_not_initialized'] = 'รอดำเนินการเริ่มต้นของผู้ใช้';
+$lang['dropbox_user_invalid'] = 'ผู้ใช้ Dropbox ไม่ถูกต้อง';
+$lang['dropbox_user_no_access'] = 'ผู้ดูแลระบบยังไม่ได้ตั้งค่าบัญชีนี้ที่มีการเข้าถึง Dropbox';
+$lang['dropbox_users'] = 'ผู้ใช้ Dropbox';
