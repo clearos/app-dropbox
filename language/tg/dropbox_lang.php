da8d805c3ef0fac197e11c0b8234d544f3be43b5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:16:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Хадамоти Dropbox банд initializing истифодабарандаи дигар аст.';
+$lang['dropbox_app_description'] = 'Dropbox хадамоти файли мизбон бо Dropbox, Inc. фаъолият, ки пешниҳод нигаҳдории абр, ҳамоҳангсозӣ файл ва нармафзор муштарӣ дорад.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Андоза папкаи';
+$lang['dropbox_missing_home_folder'] = 'Нопайдо феҳристи хонагӣ - санҷиши дастрасӣ ба хизматрасониҳои файл.';
+$lang['dropbox_no_data'] = 'Не маълумоти ...';
+$lang['dropbox_reset_complete'] = 'Dropbox папкаи маҳаллӣ маҳаллии шумо ҳазф шудааст ва ба ҳисоби аз нав маълумот Серв.';
+$lang['dropbox_reset_confirm'] = 'Тавсия дода мешавад, ки шумо ба ин дастгоҳ безорӣ аз портали Dropbox пеш азимута (тозакунии) ҷузвдони Dropbox маҳаллии кунед.';
+$lang['dropbox_reset_delete'] = 'Reset / Тозакунии Папкаи маҳаллӣ';
+$lang['dropbox_service_not_running'] = 'Хадамоти Dropbox аст, иҷро намешавад.';
+$lang['dropbox_starting_service'] = 'Шумо, ҳаргиз натавонед, ки ба оғози хизмати Dropbox ба истифодабаранда, то ҳадди ақал як ҳисоби худ танзим кардааст. Вуруд ба ҳамчун истифодакунанда (ғайри реша) Webconfig ва идора ба воситаи сабти syncronization Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'То initialization истифодабаранда';
+$lang['dropbox_user_invalid'] = 'истифодабарандаи Dropbox нодуруст аст.';
+$lang['dropbox_user_no_access'] = 'Мудири дода нашуда, то ин воқеа бо дастрасии Dropbox.';
+$lang['dropbox_users'] = 'Истифодабарандагон Dropbox';
