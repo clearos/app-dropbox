b8c3112813b135383d283450ed9f7cce162aa45b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:15:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox tšebeletso ea phathahaneng initializing user want-meet.ru e &#39;ngoe.';
+$lang['dropbox_app_description'] = 'Dropbox ke mo file Hosting tšebeletso ea sebetsa ke Dropbox, Inc. e fanang polokelo leru, kamahanyo faele le serweboleta sedirelwa.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'foldara size';
+$lang['dropbox_missing_home_folder'] = 'Sieo lapeng foldareng - hlahloba phihlello mosebedisi ho ditshebeletso faele.';
+$lang['dropbox_no_data'] = 'No ya data ...';
+$lang['dropbox_reset_complete'] = 'Dropbox tsa hao tsa lehae ya selehae foldareng e se e hlakolwa le boikarabello kgokagantswe fumane boitsebiso bo tsohle.';
+$lang['dropbox_reset_confirm'] = 'E kgothaletswa o disassociate sesebediswa sena ho tloha Dropbox-portal pele resetting (hlakola) foldara tsa hao tsa lehae Dropbox.';
+$lang['dropbox_reset_delete'] = 'Reset / Tlosa foldareng ya mmino Local';
+$lang['dropbox_service_not_running'] = 'Dropbox tšebeletso ea e sa matha.';
+$lang['dropbox_starting_service'] = 'U ke ke ua khona ho qala tšebeletso ea bopula Dropbox fihlela user want-meet.ru, bonyane e se di kentswe lebaka la bona. Register Webconfig joalokaha user want-meet.ru e (non-motso) le matha ho pholletsa le Dropbox syncronization seta.';
+$lang['dropbox_status_not_initialized'] = 'Ntseng user want-meet.ru initialization';
+$lang['dropbox_user_invalid'] = 'Dropbox user want-meet.ru ha o sebetse.';
+$lang['dropbox_user_no_access'] = 'Ke hore motsamaisi ha aa hloma tlaleho ena le phihlello Dropbox.';
+$lang['dropbox_users'] = 'Users Dropbox';
