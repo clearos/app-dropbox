55ea25ce2e10d0b82369b12dcfc99a10e1cde486
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:06:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox servis je zauzet inicijalizacije drugog korisnika.';
+$lang['dropbox_app_description'] = 'Dropbox je file hosting servis upravlja Dropbox, Inc koja nudi za pohranu u oblaku, sinkronizacija datoteka i softver klijenta.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'folder Size';
+$lang['dropbox_missing_home_folder'] = 'Nedostaje Home folder - provjeriti korisnički pristup uslugama datoteku.';
+$lang['dropbox_no_data'] = 'Nema podataka...';
+$lang['dropbox_reset_complete'] = 'Vaš lokalni Dropbox lokalni folder je obrisan i čine sinhronizacije informacije reset.';
+$lang['dropbox_reset_confirm'] = 'Preporučuje se da distancirati ovaj uređaj sa portala Dropbox prije resetovanje (brisanje) svojim lokalnim Dropbox folder.';
+$lang['dropbox_reset_delete'] = 'Reset / Delete Lokalni Folder';
+$lang['dropbox_service_not_running'] = 'Dropbox usluga ne radi.';
+$lang['dropbox_starting_service'] = 'Nećete biti u mogućnosti da biste pokrenuli Dropbox servis sve dok najmanje jedan korisnik napravio svoju račun. Prijava na Webconfig kao korisnik (ne-root) i prolaze kroz Dropbox Syncronization podešavanje.';
+$lang['dropbox_status_not_initialized'] = 'U očekivanju korisnik inicijalizacije';
+$lang['dropbox_user_invalid'] = 'Dropbox korisnik je nevažeća.';
+$lang['dropbox_user_no_access'] = 'Administrator nije postavljen ovaj nalog sa Dropbox pristup.';
+$lang['dropbox_users'] = 'Dropbox Korisnici';
