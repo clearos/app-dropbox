1c5ad017676948934b3104efbb5122a7ffb9f2ec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:11:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Animadversae est circa ministerium initializing alius user.';
+$lang['dropbox_app_description'] = 'Animadversae est operata per Dropbox file hosting, Inc. nubes repono, quod offert, synchronization et file client software.';
+$lang['dropbox_app_name'] = 'animadversae';
+$lang['dropbox_folder_size'] = 'Size folder';
+$lang['dropbox_missing_home_folder'] = 'Absentis domus folder - reprehendo file user accessum ad officia.';
+$lang['dropbox_no_data'] = 'Non data ...';
+$lang['dropbox_reset_complete'] = 'Vestri locus Dropbox loci et ratio folder deleta sync reset notitia.';
+$lang['dropbox_reset_confirm'] = 'Commendatur disassociate te de hoc artificio Dropbox ante ostium resetting (deleting) loci Dropbox folder.';
+$lang['dropbox_reset_delete'] = 'Reset / Delete Local Folder';
+$lang['dropbox_service_not_running'] = 'Animadversae servitium est non currit.';
+$lang['dropbox_starting_service'] = 'Non poteris initio liturgiae usque Dropbox saltem illorum usor felis. Ut positum Webconfig user (non- radix) et habeat syncronization Dropbox percurrere.';
+$lang['dropbox_status_not_initialized'] = 'Donec User initialization';
+$lang['dropbox_user_invalid'] = 'Animadversae nulla user est.';
+$lang['dropbox_user_no_access'] = 'Et propter hoc non suscitant Administratore Dropbox obvius.';
+$lang['dropbox_users'] = 'Users dimittere';
