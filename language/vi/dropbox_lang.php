412f98cf37d1a60e1411650050a5a15a8c2d8b62
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:17:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'dịch vụ Dropbox đang bận khởi tạo một người dùng khác.';
+$lang['dropbox_app_description'] = 'Dropbox là một tập tin lưu trữ phục vụ điều hành bởi Dropbox, Inc cung cấp lưu trữ đám mây, đồng bộ hóa tập tin và phần mềm máy khách.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'folder Size';
+$lang['dropbox_missing_home_folder'] = 'Thiếu thư mục nhà - kiểm tra người dùng truy cập đến các dịch vụ tập tin.';
+$lang['dropbox_no_data'] = 'Không có dữ liệu...';
+$lang['dropbox_reset_complete'] = 'Dropbox thư mục địa phương địa phương của bạn đã bị xóa và tài thiết lập lại các thông tin đồng bộ.';
+$lang['dropbox_reset_confirm'] = 'Chúng tôi đề nghị bạn tách thiết bị này từ cổng thông tin trước khi đặt Dropbox (xóa) thư mục Dropbox địa phương của bạn.';
+$lang['dropbox_reset_delete'] = 'Thiết lập lại / Xóa thư mục địa phương';
+$lang['dropbox_service_not_running'] = 'dịch vụ Dropbox không chạy.';
+$lang['dropbox_starting_service'] = 'Bạn sẽ không thể bắt đầu dịch vụ Dropbox cho đến ít nhất một người dùng đã cấu hình tài khoản của họ. Đăng nhập vào webconfig như một người dùng (không phải root) và chạy qua các thiết lập syncronization Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'Trong khi chờ khởi tạo người dùng';
+$lang['dropbox_user_invalid'] = 'người sử dụng Dropbox là không hợp lệ.';
+$lang['dropbox_user_no_access'] = 'Các quản trị viên chưa cài đặt tài khoản này có quyền truy cập Dropbox.';
+$lang['dropbox_users'] = 'người dùng Dropbox';
