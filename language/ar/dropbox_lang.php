5edd5af19c5b1d50d0e67cfdceac5d91f5144736
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:05:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'خدمة دروببوإكس مشغولة تهيئة مستخدم آخر.';
+$lang['dropbox_app_description'] = 'دروببوإكس هو خدمة استضافة الملفات التي تديرها دروببوإكس، Inc. أن تقدم سحابة التخزين، ومزامنة الملفات وبرنامج العميل.';
+$lang['dropbox_app_name'] = 'بصندوق الإسقاط';
+$lang['dropbox_folder_size'] = 'مجلد الحجم';
+$lang['dropbox_missing_home_folder'] = 'في عداد المفقودين المجلد الرئيسي - التحقق من وصول المستخدم إلى ملف الخدمات.';
+$lang['dropbox_no_data'] = 'لايوجد بيانات...';
+$lang['dropbox_reset_complete'] = 'لقد تم حذف مجلد محلي دروببوإكس الخاص المحلي وتمثل إعادة تعيين معلومات المزامنة.';
+$lang['dropbox_reset_confirm'] = 'فمن المستحسن أن تنأى هذا الجهاز من البوابة دروببوإكس قبل إعادة (حذف) مجلد دروببوإكس المحلي.';
+$lang['dropbox_reset_delete'] = 'إعادة تعيين / حذف المجلد المحلي';
+$lang['dropbox_service_not_running'] = 'خدمة دروببوإكس ليست قيد التشغيل.';
+$lang['dropbox_starting_service'] = 'أنت لن تكون قادرة على بدء تشغيل خدمة دروببوإكس حتى تكوينه مستخدم واحد على الأقل حسابه. تسجيل الدخول إلى Webconfig كمستخدم (غير الجذر) وتشغيل من خلال الإعداد syncronization دروببوإكس.';
+$lang['dropbox_status_not_initialized'] = 'في انتظار التهيئة المستخدم';
+$lang['dropbox_user_invalid'] = 'المستخدم دروببوإكس غير صالح.';
+$lang['dropbox_user_no_access'] = 'لم يقم بتعيين المسؤول عن هذا الحساب مع الوصول دروببوإكس.';
+$lang['dropbox_users'] = 'دروببوإكس المستخدمين';
