d6c06db6f52e3f1d9bf7b2149e5b7175d2ec73c2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:11:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'ZMI paslauga užimtas Inicijuojama kitą vartotoją.';
+$lang['dropbox_app_description'] = 'ZMI yra File hosting service valdo Dropbox, Inc., kuri siūlo debesys saugojimo, failo sinchronizavimą ir kliento programinė įranga.';
+$lang['dropbox_app_name'] = 'ZMI';
+$lang['dropbox_folder_size'] = 'Folder Size';
+$lang['dropbox_missing_home_folder'] = 'Trūksta namų aplanką - patikrinti vartotojo prieigą prie failų paslaugas.';
+$lang['dropbox_no_data'] = 'Nėra duomenų...';
+$lang['dropbox_reset_complete'] = 'Jūsų vietinis ZMI vietiniame aplanke buvo ištrinta ir paskyros sinchronizavimo informacija atkūrimas.';
+$lang['dropbox_reset_confirm'] = 'Patartina tai atsieti šį įrenginį iš Dropbox portalo Prieš iš naujo (ištrinkite) savo vietos Dropbox aplanką.';
+$lang['dropbox_reset_delete'] = 'Atstatyti / Trinti vietiniame aplanke';
+$lang['dropbox_service_not_running'] = 'ZMI paslauga neveikia.';
+$lang['dropbox_starting_service'] = 'Jūs negalite pradėti Dropbox tarnyba, kol bent vienas vartotojas neužpildė savo paskyros. Prisijunk prie Webconfig kaip vartotojo (ne root) ir paleisti per Dropbox sinchronizavimą setup.';
+$lang['dropbox_status_not_initialized'] = 'Kol vartotojas iniciacijos';
+$lang['dropbox_user_invalid'] = 'ZMI vartotojas yra negaliojantis.';
+$lang['dropbox_user_no_access'] = 'Administratorius nėra nustatyti šią sąskaitą Dropbox prieigos.';
+$lang['dropbox_users'] = 'Dropbox Vartotojai';
