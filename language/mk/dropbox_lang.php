3fac7048d623a88bbd9c18c2aae7a425bb5971d7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:12:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox услуга е зафатен иницијализација на друг корисник.';
+$lang['dropbox_app_description'] = 'Dropbox е сервис датотека хостинг управувана од страна на Dropbox, Inc, кој нуди облак складирање, синхронизација на датотеки и клиент софтвер.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'папка Големина';
+$lang['dropbox_missing_home_folder'] = 'Недостасува домашна папка - проверете корисникот пристап до датотеки услуги.';
+$lang['dropbox_no_data'] = 'Нема податок...';
+$lang['dropbox_reset_complete'] = 'Вашата локална Dropbox локална папка е избришан и сметка за синхронизација информации ресетирање.';
+$lang['dropbox_reset_confirm'] = 'Препорачливо е да се дистанцираат овој уред од порталот Dropbox пред ресетирам (бришење) на вашата локална папка Dropbox.';
+$lang['dropbox_reset_delete'] = 'Ресетирате / Избриши локална папка';
+$lang['dropbox_service_not_running'] = 'Dropbox сервис не работи.';
+$lang['dropbox_starting_service'] = 'Вие нема да бидете во можност да се започне на услугата Dropbox барем еден корисник се подготвени за нивна сметка. Влези за да го Webconfig како корисник (не-root) и работи преку поставување на syncronization Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'Во очекување на корисник иницијализација';
+$lang['dropbox_user_invalid'] = 'Dropbox корисникот не е валиден.';
+$lang['dropbox_user_no_access'] = 'Администраторот не е го постави оваа сметка со Dropbox пристап.';
+$lang['dropbox_users'] = 'Dropbox корисници';
