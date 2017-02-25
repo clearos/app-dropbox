e98436c05994e903934c0c3d321b9c233f465e85
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:17:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'gwasanaeth Dropbox yn brysur ymgychwyn ddefnyddiwr arall.';
+$lang['dropbox_app_description'] = 'Dropbox yn wasanaeth ffeil cynnal a weithredir gan Dropbox, Inc sy&#39;n cynnig storio cwmwl, synchronization ffeil a meddalwedd cleient.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Maint folder';
+$lang['dropbox_missing_home_folder'] = 'Ar goll plygell cartref - gwiriwch mynediad defnyddwyr at wasanaethau ffeil.';
+$lang['dropbox_no_data'] = 'Dim data ...';
+$lang['dropbox_reset_complete'] = 'Eich ffolder lleol Dropbox lleol wedi cael ei ddileu a rhoi cyfrif ailosod gwybodaeth cydamseru.';
+$lang['dropbox_reset_confirm'] = 'Rydym yn argymell eich datgysylltu ddyfais hon gan y porth Dropbox cyn ailosod (dileu) eich ffolder Dropbox lleol.';
+$lang['dropbox_reset_delete'] = 'Ailosod / Dileu Ffolder Lleol';
+$lang['dropbox_service_not_running'] = 'Nid yw gwasanaeth Dropbox yn rhedeg.';
+$lang['dropbox_starting_service'] = 'Ni fyddwch yn gallu cychwyn y gwasanaeth Dropbox tan o leiaf un defnyddiwr wedi ffurfweddu eu cyfrif. Mewngofnodi i Webconfig fel defnyddiwr (heb fod yn gwraidd) ac yn rhedeg drwy&#39;r setup syncronization Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'Tra&#39;n aros initialization defnyddiwr';
+$lang['dropbox_user_invalid'] = 'defnyddiwr Dropbox yn annilys.';
+$lang['dropbox_user_no_access'] = 'Nid yw&#39;r gweinyddwr wedi sefydlu cyfrif hwn gyda mynediad Dropbox.';
+$lang['dropbox_users'] = 'Defnyddwyr Dropbox';
