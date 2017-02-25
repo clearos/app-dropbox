10233b1d8d780cb19505967c4517d4919fe34075
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:05:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Shërbimi Dropbox është i zënë fillimin një përdorues.';
+$lang['dropbox_app_description'] = 'Dropbox është një shërbim fotografi hosting operuar nga Dropbox, Inc se ofron magazinimit cloud, sinkronizimi file dhe software klientit.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Size Folder';
+$lang['dropbox_missing_home_folder'] = 'Missing dosje në shtëpi - të kontrolluar aksesin e përdoruesit për shërbimet e file.';
+$lang['dropbox_no_data'] = 'Nuk ka të dhëna...';
+$lang['dropbox_reset_complete'] = 'Dropbox dosjen tuaj lokale lokale është fshirë dhe llogari rivendosur sync informacionit.';
+$lang['dropbox_reset_confirm'] = 'Është e rekomanduar që ju të shkëputet këtë pajisje nga portali Dropbox para adaptimet (fshirjes) tuaj dosje lokale Dropbox.';
+$lang['dropbox_reset_delete'] = 'Reset / Delete Folder Lokale';
+$lang['dropbox_service_not_running'] = 'Shërbimi Dropbox nuk është i rrjedhshëm.';
+$lang['dropbox_starting_service'] = 'Ju nuk do të jetë në gjendje për të filluar shërbimin Dropbox deri të paktën një përdorues ka konfiguruar llogarinë e tyre. Identifikohuni për Webconfig si një përdorues (jo-rrënjë) dhe të drejtuar përmes setup syncronization Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'Në pritje initialization përdorues';
+$lang['dropbox_user_invalid'] = 'përdorues i Dropbox është e pavlefshme.';
+$lang['dropbox_user_no_access'] = 'Administratori nuk e ka ngritur këtë llogari me qasje Dropbox.';
+$lang['dropbox_users'] = 'Përdoruesit Dropbox';
