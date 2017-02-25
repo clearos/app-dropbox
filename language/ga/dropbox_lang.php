015c2c85f4260de8b81f8e22f9c5c3bc5d3f7d29
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:10:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Tá seirbhís Dropbox gnóthach initializing úsáideoir eile.';
+$lang['dropbox_app_description'] = 'Is Dropbox seirbhís comhad a óstáil oibriú ag Dropbox, Inc. go dtugann stórála scamall, sioncrónaithe comhad agus bogearraí cliant.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'fillteán Méid';
+$lang['dropbox_missing_home_folder'] = 'Iarraidh fillteán baile - rochtana úsáideora seiceáil ar sheirbhísí comhad.';
+$lang['dropbox_no_data'] = 'Aon sonraí ...';
+$lang['dropbox_reset_complete'] = 'Tá do fillteán áitiúil Dropbox áitiúil scriosadh agus athshocrú eolais info cuntas.';
+$lang['dropbox_reset_confirm'] = 'Tá sé molta tú disassociate an gléas as an tairseach Dropbox sula resetting (scriosadh) do fhillteán Dropbox áitiúil.';
+$lang['dropbox_reset_delete'] = 'Athshocraigh / Scrios Fillteán Áitiúil';
+$lang['dropbox_service_not_running'] = 'Níl an tseirbhís Dropbox ag rith.';
+$lang['dropbox_starting_service'] = 'Ní bheidh tú in ann tús a chur leis an tseirbhís Dropbox go dtí go mbeidh úsáideoir amháin ar a laghad cumraithe a gcuntas. Logáil isteach chun Webconfig mar úsáideoir (neamh-fréimhe) agus a reáchtáil trí mheán an thus syncronization Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'Go dtí go initialization úsáideora';
+$lang['dropbox_user_invalid'] = 'Is úsáideora Dropbox neamhbhailí.';
+$lang['dropbox_user_no_access'] = 'Níor chuir an riarthóir ar bun an cuntas le rochtain Dropbox.';
+$lang['dropbox_users'] = 'Úsáideoirí Dropbox';
