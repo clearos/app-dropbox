482b2e14efa5b843649728516366fcad5e0795c2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:07:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox的服务正忙初始化其他用户。';
+$lang['dropbox_app_description'] = 'Dropbox是Dropbox的由公司运营的网络硬盘，提供云存储，文件同步和客户端软件。';
+$lang['dropbox_app_name'] = 'Dropbox的';
+$lang['dropbox_folder_size'] = '文件夹大小';
+$lang['dropbox_missing_home_folder'] = '缺少主文件夹 - 检查文件服务的用户访问。';
+$lang['dropbox_no_data'] = '没有数据...';
+$lang['dropbox_reset_complete'] = '您当地的Dropbox本地文件夹已被删除和帐户同步信息复位。';
+$lang['dropbox_reset_confirm'] = '建议您从Dropbox的门户网站取消这一设备重置（删除）在本地的Dropbox文件夹之前。';
+$lang['dropbox_reset_delete'] = '复位/删除本地文件夹';
+$lang['dropbox_service_not_running'] = 'Dropbox的服务没有运行。';
+$lang['dropbox_starting_service'] = '您将无法启动Dropbox的服务，直到至少一个用户配置他们的帐户。 登录Webconfig作为一个用户（非root），并通过Dropbox的同步化设置运行。';
+$lang['dropbox_status_not_initialized'] = '待用户初始化';
+$lang['dropbox_user_invalid'] = 'Dropbox的用户是无效的。';
+$lang['dropbox_user_no_access'] = '管理员没有设置此帐户与Dropbox的访问。';
+$lang['dropbox_users'] = 'Dropbox的用户';
