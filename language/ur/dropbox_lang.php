c2d1ad6d81c8be61d02244080fa12e946881b456
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:17:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'ڈراپ باکس سروس کسی دوسرے صارف ابتدا میں مصروف ہے.';
+$lang['dropbox_app_description'] = 'ڈراپ باکس ڈراپ باکس، انکارپوریٹڈ کی طرف سے آپریشن ایک فائل ہوسٹنگ سروس بادل سٹوریج، فائل ہم آہنگی اور کلائنٹ سافٹ ویئر فراہم کرتا ہے.';
+$lang['dropbox_app_name'] = 'ڈراپ باکس';
+$lang['dropbox_folder_size'] = 'فولڈر کا سائز';
+$lang['dropbox_missing_home_folder'] = 'ہوم فولڈر غائب - فائل خدمات پر صارف کی رسائی کی جانچ.';
+$lang['dropbox_no_data'] = 'کوئی مواد نہیں...';
+$lang['dropbox_reset_complete'] = 'آپ کی مقامی ڈراپ باکس مقامی فولڈر کو خارج کر دیا اور اکاؤنٹ کی مطابقت پذیری میں معلومات ری سیٹ کر دیا گیا ہے.';
+$lang['dropbox_reset_confirm'] = 'اس ری سیٹ (حذف کرنا) اپنے مقامی ڈراپ باکس کے فولڈر سے پہلے آپ ڈراپ باکس پورٹل سے اس آلہ علیحدہ سفارش کی جاتی ہے.';
+$lang['dropbox_reset_delete'] = 'ری سیٹ کریں / مقامی فولڈر حذف';
+$lang['dropbox_service_not_running'] = 'ڈراپ باکس سروس نہیں چل رہا ہے.';
+$lang['dropbox_starting_service'] = 'جب تک کم از کم ایک صارف ان کے اکاؤنٹ ترتیب دے دیا ہے آپ ڈراپ باکس سروس شروع کرنے کے قابل نہیں ہو گا. ایک صارف (غیر جڑ) کے طور پر Webconfig کرنے اور ڈراپ باکس syncronization سیٹ اپ کے ذریعے چلانے لاگ ان کریں.';
+$lang['dropbox_status_not_initialized'] = 'زیر التوا صارف ابتدا';
+$lang['dropbox_user_invalid'] = 'ڈراپ باکس صارف غلط ہے.';
+$lang['dropbox_user_no_access'] = 'منتظم سے ڈراپ باکس تک رسائی کے ساتھ اس اکاؤنٹ کو قائم نہیں کیا ہے اپ.';
+$lang['dropbox_users'] = 'ڈراپ باکس صارفین';
