9ebdb0e3b74988c0bf4363bbcc87e6518351e343
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:06:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox xidmət başqa bir istifadəçi başlatılıyor məşğul olur.';
+$lang['dropbox_app_description'] = 'Dropbox bulud saxlama, fayl synchronization və müştəri proqram təklif edir Dropbox, Inc tərəfindən idarə bir fayl hosting xidmət edir.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Qovluq Size';
+$lang['dropbox_missing_home_folder'] = 'ev qovluq itkin - Fayl xidmətləri istifadəçi girişi edin.';
+$lang['dropbox_no_data'] = 'Məlumat yoxdur...';
+$lang['dropbox_reset_complete'] = 'Yerli Dropbox yerli qovluq silinmiş və sync məlumat yenidən haqq-hesab edilmişdir.';
+$lang['dropbox_reset_confirm'] = 'Bu (silmə) yerli Dropbox qovluq sıfırlama əvvəl Dropbox portal bu cihaz disassociate tövsiyə olunur.';
+$lang['dropbox_reset_delete'] = '/ Reset Local Qovluq Sil';
+$lang['dropbox_service_not_running'] = 'Dropbox xidmət çalışan deyil.';
+$lang['dropbox_starting_service'] = 'ən azı bir istifadəçi hesabı konfiqurasiya qədər Siz Dropbox xidmət başlamaq üçün edə bilməyəcək. bir istifadəçi (qeyri-root) kimi Webconfig və Dropbox Sinxronizasiya quraşdırma axır Giriş.';
+$lang['dropbox_status_not_initialized'] = 'Bekleyen istifadəçi başlatma';
+$lang['dropbox_user_invalid'] = 'Dropbox istifadəçi yanlışdır.';
+$lang['dropbox_user_no_access'] = 'administrator Dropbox çıxışı ilə bu hesabı yaratmaq deyil.';
+$lang['dropbox_users'] = 'Dropbox İstifadəçilər';
