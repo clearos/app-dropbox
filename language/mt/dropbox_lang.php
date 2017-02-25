94d984764177a946f1a0e693befbf8e139ec329e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:13:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'servizz Dropbox hija okkupata initializing utent ieħor.';
+$lang['dropbox_app_description'] = 'Dropbox huwa fajl hosting servizz operat minn Dropbox, Inc li joffri ħażna sħaba, fajl sinkronizzazzjoni u software klijent.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'folder Daqs';
+$lang['dropbox_missing_home_folder'] = 'Nieqsa folder dar - jiċċekkjaw aċċess għall-utenti għas-servizzi tal-fajl.';
+$lang['dropbox_no_data'] = 'M&#39;hemmx data ...';
+$lang['dropbox_reset_complete'] = 'lokali Dropbox folder lokali tiegħek ġie mħassar u kont reset informazzjoni sync.';
+$lang['dropbox_reset_confirm'] = 'Huwa rakkomandat li inti tiddissassoċja dan l-apparat mis-portal Dropbox qabel jerġa &#39;jiġi issettjat (tħassir)-folder tiegħek Dropbox lokali.';
+$lang['dropbox_reset_delete'] = 'Irrisettja / Ħassar Folder Lokali';
+$lang['dropbox_service_not_running'] = 'servizz Dropbox ma tkunx qed taħdem.';
+$lang['dropbox_starting_service'] = 'Inti mhux se tkun tista &#39;tibda s-servizz Dropbox sal utent mill-inqas wieħed ikun konfigurati kont tagħhom. Login għal Webconfig bħala utent (mhux għerq) u jgħaddi mill-setup syncronization Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'Fl-istennija inizjalizzazzjoni utent';
+$lang['dropbox_user_invalid'] = 'utent Dropbox huwa invalidu.';
+$lang['dropbox_user_no_access'] = 'L-amministratur ma waqqfet dan il-kont b&#39;aċċess Dropbox.';
+$lang['dropbox_users'] = 'Utenti Dropbox';
