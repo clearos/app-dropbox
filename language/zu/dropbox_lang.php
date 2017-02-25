d4c73b0200242de7a429772f72532bb27b898664
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:18:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Isevisi Dropbox umatasa iqala omunye umsebenzisi.';
+$lang['dropbox_app_description'] = 'Eyinqolobane iyisevisi ifayela ibamba eziqhutshwa eyinqolobane, Inc. enikeza isitoreji ifu, ifayela ukuvumelanisa futhi software iklayenti.';
+$lang['dropbox_app_name'] = 'eyinqolobane';
+$lang['dropbox_folder_size'] = 'Ifolda Usayizi';
+$lang['dropbox_missing_home_folder'] = 'Missing ifolda ekhaya - hlola ukufinyelela komsebenzisi services ifayela.';
+$lang['dropbox_no_data'] = 'Ayikho idatha ...';
+$lang['dropbox_reset_complete'] = 'Dropbox ifolda Your lendawo wendawo isisusiwe-akhawunti ukuvumelanisa imininingwane reset.';
+$lang['dropbox_reset_confirm'] = 'Kuyakhuthazwa naleli idivayisi kusukela eyinqolobane portal ngaphambi yokusetha kabusha (kokususa) ifolda eyinqolobane wangakini.';
+$lang['dropbox_reset_delete'] = 'Setha kabusha / Susa Ifolda Local';
+$lang['dropbox_service_not_running'] = 'Isevisi Dropbox akasebenzisi.';
+$lang['dropbox_starting_service'] = 'Ngeke bakwazi ukuqala inkonzo eyinqolobane kuze kube yilapho okungenani oyedwa umsebenzisi amisiwe akhawunti abo. Ngena ngemvume ukuze Webconfig njengoba umsebenzisi (non-root) bese ugijime ngokusebenzisa eyinqolobane syncronization isethaphu.';
+$lang['dropbox_status_not_initialized'] = 'Lapho esalindele Ukuqalisa umsebenzisi';
+$lang['dropbox_user_invalid'] = 'Eyinqolobane umsebenzisi ayivumelekile.';
+$lang['dropbox_user_no_access'] = 'Umlawuli engazange wamisa kule akhawunti eyinqolobane ukufinyelela.';
+$lang['dropbox_users'] = 'eyinqolobane Abasebenzisi';
