31f88ca8a78af9f9e17ea0aa3a756b050edd4fda
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:12:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox fanompoana dia sahirana nitranga teo am-mpampiasa iray hafa.';
+$lang['dropbox_app_description'] = 'Dropbox dia rakitra fampiantranoana fanompoana niasa amin&#39;ny Dropbox, Inc. izay manome rahona fitehirizana, rakitra synchronization sy ny mpanjifa rindrambaiko.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'lahatahiry Size';
+$lang['dropbox_missing_home_folder'] = 'Nanjavona an-trano lahatahiry - jereo mpampiasa ny fidirana amin&#39;ny rakitra tolotra.';
+$lang['dropbox_no_data'] = 'Tsy misy tahirin-kevitra ...';
+$lang['dropbox_reset_complete'] = 'Ny eo an-toerana eo an-toerana fampirimana Dropbox efa voafafa sy handrindra kaonty Reset vaovao.';
+$lang['dropbox_reset_confirm'] = 'Izany no soso-kevitra izao ianao disassociate fitaovana avy amin&#39;ny Dropbox vavahadin-tserasera alohan&#39;ny resetting (famafana) ny eo an-toerana fampirimana Dropbox.';
+$lang['dropbox_reset_delete'] = 'Reset / Fafao Local Folder';
+$lang['dropbox_service_not_running'] = 'Dropbox fanompoana dia tsy mihazakazaka.';
+$lang['dropbox_starting_service'] = 'Tsy ho afaka manomboka ny Dropbox mandra-fanompoana, fara fahakeliny, mpampiasa iray dia voaendrika ny kaontiny. Login ny Webconfig toy ny mpampiasa (tsy faka) ary hihazakazaka amin&#39;ny alalan&#39;ny fanamboarana syncronization Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'Miandry mpampiasa initialization';
+$lang['dropbox_user_invalid'] = 'Dropbox mpampiasa dia tsy manan-kery.';
+$lang['dropbox_user_no_access'] = 'Ny mpandrindra tsy nanangana ity tantara ity amin&#39;ny Dropbox fidirana.';
+$lang['dropbox_users'] = 'Dropbox Users';
