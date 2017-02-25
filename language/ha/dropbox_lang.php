6e1d44a24d4e1d1581050e7daf1f3fe4a14250c7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:09:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox sabis ne m soma wani mai amfani.';
+$lang['dropbox_app_description'] = 'Dropbox ne fayil hosting sabis sarrafa by Dropbox, Inc. cewa yayi girgije ajiya, file aiki tare da kuma abokin ciniki software.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Jaka Size';
+$lang['dropbox_missing_home_folder'] = 'Babu gida fayil - duba mai amfani access to file ayyuka.';
+$lang['dropbox_no_data'] = 'No data ...';
+$lang['dropbox_reset_complete'] = 'Your gida Dropbox gida fayil An share kuma asusu Daidaita bayanai sake saiti.';
+$lang['dropbox_reset_confirm'] = 'An shawarar ka barranta da wannan na&#39;ura daga Dropbox portal da resetting (share) gida Dropbox fayil.';
+$lang['dropbox_reset_delete'] = 'Sake saita / Share Local Jaka';
+$lang['dropbox_service_not_running'] = 'Dropbox sabis ba a guje.';
+$lang['dropbox_starting_service'] = 'Ba za ka iya fara Dropbox sabis har a kalla daya amfani ya kaga sabõda su. Login Webconfig a matsayin mai amfani (ba-tushen) da gudu ta hanyar Dropbox syncronization saitin.';
+$lang['dropbox_status_not_initialized'] = 'Lokacin amfani initialization';
+$lang['dropbox_user_invalid'] = 'Dropbox mai amfani ba daidai ba ne.';
+$lang['dropbox_user_no_access'] = 'The shugaba bai kafa wannan asusu tare da Dropbox access.';
+$lang['dropbox_users'] = 'Dropbox Users';
