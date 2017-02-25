24d20010901bbacdd3dc3f4b19d51d231d1c9c4d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:12:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'He pukumahi arawhiti tetahi kaiwhakamahi ratonga Dropbox.';
+$lang['dropbox_app_description'] = 'Dropbox Ko te ratonga kōnae hautū whakahaeretia e Dropbox, Inc. e tuku kapua rokiroki, tukutahitanga kōnae me te pūmanawa kiritaki.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Rahi kōpaki';
+$lang['dropbox_missing_home_folder'] = 'Ngaro kōpaki kāinga - tirohia te uru kaiwhakamahi ki ngā ratonga kōnae.';
+$lang['dropbox_no_data'] = 'Kāore he raraunga ...';
+$lang['dropbox_reset_complete'] = 'Kua tou kōpaki rohe Dropbox rohe kua mukua me pūkete tukutahi mōhiohio tautuhi.';
+$lang['dropbox_reset_confirm'] = 'e tūtohu ana disassociate koe i tenei pūrere i te tomokanga Dropbox i mua i te tautuhi anō (te muku) tō kōpaki Dropbox rohe.';
+$lang['dropbox_reset_delete'] = 'Tautuhi anō / Muku Kōpaki Paetata';
+$lang['dropbox_service_not_running'] = 'e kore te rere ratonga Dropbox.';
+$lang['dropbox_starting_service'] = 'e kore e waiho e koe taea ki te tīmata i te ratonga Dropbox kua whirihora noa i te iti rawa kia kotahi te kaiwhakamahi ratou pūkete. Takiuru ki te Webconfig rite te kaiwhakamahi (pakiaka-kore), ka rere i roto i te tatūnga syncronization Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'Tārewa arawhiti kaiwhakamahi';
+$lang['dropbox_user_invalid'] = 'He muhu Dropbox kaiwhakamahi.';
+$lang['dropbox_user_no_access'] = 'E kore Kua te kaiwhakahaere whakaturia ake tenei pūkete ki Dropbox uru.';
+$lang['dropbox_users'] = 'Kaiwhakamahi Dropbox';
