a0911f7eea2238f83b2e876cbe8bee6fc0334d0e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:16:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox serbisyo ay busy Sinisimulan isa pang user.';
+$lang['dropbox_app_description'] = 'Dropbox ay isang file hosting service pinamamahalaan sa pamamagitan ng Dropbox, Inc. na nag-aalok ng ulap imbakan, file synchronization at client software.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'folder Sukat';
+$lang['dropbox_missing_home_folder'] = 'Nawawalang home folder - suriin user ng access sa mga serbisyo ng file.';
+$lang['dropbox_no_data'] = 'Walang data ...';
+$lang['dropbox_reset_complete'] = 'Ang iyong lokal na Dropbox lokal na folder ay nabura at account sync impormasyon reset.';
+$lang['dropbox_reset_confirm'] = 'Ito ay inirerekomenda mong iugnay aparatong ito mula sa Dropbox portal bago i-reset (pagtanggal) ang iyong lokal na folder Dropbox.';
+$lang['dropbox_reset_delete'] = 'I-reset / Burahin Lokal Folder';
+$lang['dropbox_service_not_running'] = 'Dropbox serbisyo ay hindi tumatakbo.';
+$lang['dropbox_starting_service'] = 'Ikaw ay hindi magagawang upang simulan ang Dropbox serbisyo hanggang hindi bababa sa isang user ay naka-configure ang kanilang account. Mag-login sa Webconfig bilang isang gumagamit (non-root) at patakbuhin sa pamamagitan ng syncronization setup Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'Nakabinbin user initialization';
+$lang['dropbox_user_invalid'] = 'Dropbox user ay hindi wasto.';
+$lang['dropbox_user_no_access'] = 'administrator ay hindi-set up ang account na ito sa Dropbox access.';
+$lang['dropbox_users'] = 'Dropbox Users';
