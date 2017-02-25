0976d0ee4c4e0fc7aaeb0ea93f3f452f566e8d92
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:11:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'ການບໍລິການ Dropbox ເປັນທຸລະກິດການເລີ່ມຕົ້ນຂອງຜູ້ໃຊ້ອື່ນ.';
+$lang['dropbox_app_description'] = 'Dropbox ເປັນເອກະສານບໍລິການໂຮດດໍາເນີນການໂດຍ Dropbox, Inc ທີ່ມີບໍລິການເກັບຮັກສາຟັງ, synchronization ໄຟລ໌ແລະຊອບແວລູກຄ້າ.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'ຂະຫນາດໂຟເດີ';
+$lang['dropbox_missing_home_folder'] = 'ທີ່ຂາດຫາຍໄປໂຟນເດີບ້ານ - ກວດເບິ່ງການເຂົ້າເຖິງຜູ້ໃຊ້ການບໍລິການເອກະສານ.';
+$lang['dropbox_no_data'] = 'ບໍ່​ມີ​ຂໍ້​ມູນ...';
+$lang['dropbox_reset_complete'] = 'ໂຟນເດີໃນທ້ອງຖິ່ນ Dropbox ທ້ອງຖິ່ນຂອງທ່ານໄດ້ຖືກລຶບອອກແລ້ວແລະບັນຊີການປັບຂໍ້ມູນ sync.';
+$lang['dropbox_reset_confirm'] = 'ມັນໄດ້ຖືກແນະນໍາໃຫ້ທ່ານລົງຄວາມເຫັນນໍາອຸປະກອນນີ້ຈາກປະຕູ Dropbox ກ່ອນການຕັ້ງຄ່າ (ລົບ) folder Dropbox ທ້ອງຖິ່ນຂອງທ່ານ.';
+$lang['dropbox_reset_delete'] = 'ປັບ / ລົບ Folder ທ້ອງຖິ່ນ';
+$lang['dropbox_service_not_running'] = 'ການບໍລິການ Dropbox ບໍ່ໄດ້ເຮັດວຽກ.';
+$lang['dropbox_starting_service'] = 'ທ່ານຈະບໍ່ສາມາດເພື່ອເລີ່ມຕົ້ນການບໍລິການ Dropbox ຈົນກ່ວາຜູ້ໃຊ້ຢ່າງຫນ້ອຍຫນຶ່ງກໍາຫນົດບັນຊີຂອງເຂົາເຈົ້າ. ເຂົ້າສູ່ລະບົບເພື່ອ Webconfig ເປັນສະມາຊິກ (ບໍ່ແມ່ນ root) ແລະດໍາເນີນການໂດຍຜ່ານການຕິດຕັ້ງ syncronous Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'ລໍຖ້າການເລີ່ມຕົ້ນຜູ້ໃຊ້';
+$lang['dropbox_user_invalid'] = 'ຜູ້ໃຊ້ Dropbox ແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['dropbox_user_no_access'] = 'ຜູ້ບໍລິຫານຍັງບໍ່ທັນໄດ້ສ້າງຕັ້ງຂຶ້ນບັນຊີນີ້ມີການເຂົ້າເຖິງ Dropbox.';
+$lang['dropbox_users'] = 'ຜູ້ໃຊ້ Dropbox';
