71453b137fdd10917f99eb5c2c9075a191be6848
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:11:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'សេវាកម្ម Dropbox គឺរវល់ចាប់ផ្តើមប្រើផ្សេងទៀត។';
+$lang['dropbox_app_description'] = 'Dropbox គឺជាសេវាបង្ហោះឯកសារដែលបានដំណើរការដោយ Dropbox, Inc ដែលផ្តល់ការផ្ទុកពពក, ធ្វើសមកាលកម្មឯកសារនិងកម្មវិធីម៉ាស៊ីនភ្ញៀវ។';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'ទំហំថត';
+$lang['dropbox_missing_home_folder'] = 'បាត់ខ្លួនក្នុងថតផ្ទះ - ពិនិត្យមើលសិទ្ធិចូលដំណើរការរបស់អ្នកប្រើសេវាការឯកសារ។';
+$lang['dropbox_no_data'] = 'គ្មាន​ទិន្នន័យ...';
+$lang['dropbox_reset_complete'] = 'ថតមូលដ្ឋាន Dropbox របស់មូលដ្ឋានរបស់អ្នកត្រូវបានលុបចេញហើយកំណត់ឡើងវិញពធ្វើសមកាលកម្មគណនី។';
+$lang['dropbox_reset_confirm'] = 'វាត្រូវបានផ្ដល់អនុសាសន៍ឱ្យអ្នកផ្ដាច់ឧបករណ៍នេះពីមុនពេលកំណត់វិបផតថល Dropbox (លុប) ថត Dropbox មូលដ្ឋានរបស់អ្នក។';
+$lang['dropbox_reset_delete'] = 'កំណត់ឡើងវិញ / លុបថតក្នុងស្រុក';
+$lang['dropbox_service_not_running'] = 'សេវាកម្ម Dropbox មិនកំពុងរត់។';
+$lang['dropbox_starting_service'] = 'អ្នកនឹងមិនអាចចាប់ផ្តើមសេវា Dropbox នេះរហូតដល់អ្នកប្រើយ៉ាងហោចណាស់មួយត្រូវបានកំណត់រចនាសម្ព័ន្ធគណនីរបស់ពួកគេ។ ចូលដើម្បី Webconfig ថាជាអ្នកប្រើ (ដែលមិនមែនជា root) និងរត់តាមរយៈការរៀបចំធ្វើសមកាលកម្ម Dropbox ។';
+$lang['dropbox_status_not_initialized'] = 'ការរង់ចាំការចាប់ផ្ដើមរបស់អ្នកប្រើ';
+$lang['dropbox_user_invalid'] = 'អ្នកប្រើ Dropbox គឺមិនត្រឹមត្រូវ។';
+$lang['dropbox_user_no_access'] = 'អ្នកគ្រប់គ្រងមិនបានរៀបចំគណនីជាមួយ Dropbox ការចូលដំណើរការនេះ។';
+$lang['dropbox_users'] = 'អ្នកប្រើ Dropbox';
