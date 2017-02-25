743272b6780fd75c17d98dca964945d114886a12
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:12:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox pakalpojums ir aizņemts inicializēšana citu lietotāju.';
+$lang['dropbox_app_description'] = 'Dropbox ir failu hostinga pakalpojumu vada Dropbox, Inc., kas piedāvā mākonis uzglabāšanas, failu sinhronizāciju un klienta programmatūru.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Mapes lielums';
+$lang['dropbox_missing_home_folder'] = 'Trūkst mājas mapi - pārbaudiet lietotāja piekļuvi failu pakalpojumiem.';
+$lang['dropbox_no_data'] = 'Nav datu...';
+$lang['dropbox_reset_complete'] = 'Jūsu vietējā Dropbox vietējā mape ir svītrots un konta sinhronizācijas informāciju reset.';
+$lang['dropbox_reset_confirm'] = 'Ieteicams jūs nošķirt šo ierīci no Dropbox portāla pirms atiestatīšanas (dzēst) vietējo Dropbox mapi.';
+$lang['dropbox_reset_delete'] = 'Reset / dzēšana vietējo mapi';
+$lang['dropbox_service_not_running'] = 'Dropbox pakalpojums nedarbojas.';
+$lang['dropbox_starting_service'] = 'Jūs nevarēsiet sākt Dropbox pakalpojumu līdz vismaz viens lietotājs ir konfigurējis savu kontu. Ieeja Webconfig kā lietotāja (ne-root) un palaist caur Dropbox syncronization setup.';
+$lang['dropbox_status_not_initialized'] = 'Kamēr lietotājs inicializācijas';
+$lang['dropbox_user_invalid'] = 'Dropbox lietotājs ir nederīgs.';
+$lang['dropbox_user_no_access'] = 'Administrators nav izveidojusi šo kontu ar Dropbox piekļuvi.';
+$lang['dropbox_users'] = 'Dropbox lietotāji';
