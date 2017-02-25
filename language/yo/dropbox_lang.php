237b78a9612c83f92f7d18f0f6b896b9889bc66f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:17:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox iṣẹ ni o nšišẹ initializing miran olumulo.';
+$lang['dropbox_app_description'] = 'Dropbox jẹ a file alejo iṣẹ ṣiṣẹ nipa Dropbox, Inc. ti o nfun awọsanma ipamọ, file amuṣiṣẹpọ ati ni ose software.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'folda Iwon';
+$lang['dropbox_missing_home_folder'] = 'Sonu ile folda - ṣayẹwo olumulo wiwọle si faili iṣẹ.';
+$lang['dropbox_no_data'] = 'Ko si data ...';
+$lang['dropbox_reset_complete'] = 'Ti agbegbe rẹ Dropbox agbegbe folda ti a ti paarẹ ki o si iroyin ìsiṣẹpọ alaye si ipilẹ.';
+$lang['dropbox_reset_confirm'] = 'O ti wa ni niyanju o disassociate yi ẹrọ lati Dropbox portal ṣaaju ki o to ntun (pipaarẹ) ti agbegbe rẹ Dropbox folda.';
+$lang['dropbox_reset_delete'] = 'Tun / Pa Agbegbe Folda';
+$lang['dropbox_service_not_running'] = 'Dropbox iṣẹ ti wa ni ko nṣiṣẹ.';
+$lang['dropbox_starting_service'] = 'O yoo ko ni anfani lati bẹrẹ awọn Dropbox iṣẹ titi ni o kere kan olulo ti ni tunto wọn iroyin. Buwolu wọle lati Webconfig bi a olumulo (ti kii-root) ati ṣiṣe awọn nipasẹ awọn Dropbox syncronization setup.';
+$lang['dropbox_status_not_initialized'] = 'Ni isunmọtosi ni olumulo initialization';
+$lang['dropbox_user_invalid'] = 'Dropbox olumulo ti wa ni invalid.';
+$lang['dropbox_user_no_access'] = 'Awọn administrator ti ko ṣeto soke yi iroyin pẹlu Dropbox wiwọle.';
+$lang['dropbox_users'] = 'Dropbox oníṣe';
