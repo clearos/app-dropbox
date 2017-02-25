e3a39824f0eee8f85c29116a4474280f80c60022
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:13:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox service is bezig initialiseren van een andere gebruiker.';
+$lang['dropbox_app_description'] = 'Dropbox is een file hosting dienst van Dropbox, Inc. dat cloud storage, file synchronisatie en client-software biedt.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'folder Size';
+$lang['dropbox_missing_home_folder'] = 'Ontbrekende thuismap - controleer de gebruiker toegang tot het dossier van diensten.';
+$lang['dropbox_no_data'] = 'Geen gegevens...';
+$lang['dropbox_reset_complete'] = 'Uw lokale lokale Dropbox-map is verwijderd en Accountsynchronisatie informatie reset.';
+$lang['dropbox_reset_confirm'] = 'Het is raadzaam dit apparaat te distantiëren van de Dropbox-portaal voor het resetten (verwijderen) van uw lokale map Dropbox.';
+$lang['dropbox_reset_delete'] = 'Reset / verwijderen lokale map';
+$lang['dropbox_service_not_running'] = 'Dropbox service is niet actief.';
+$lang['dropbox_starting_service'] = 'Je zal niet in staat zijn om de Dropbox-service te starten tot ten minste één gebruiker zijn account heeft geconfigureerd. Meld u aan om WEBconfig als gebruiker (niet-root) en lopen door de Dropbox synchronisatie setup.';
+$lang['dropbox_status_not_initialized'] = 'In afwachting van de gebruiker initialisatie';
+$lang['dropbox_user_invalid'] = 'Dropbox gebruiker is ongeldig.';
+$lang['dropbox_user_no_access'] = 'De beheerder is niet opgezet deze rekening met Dropbox toegang.';
+$lang['dropbox_users'] = 'Dropbox Gebruikers';
