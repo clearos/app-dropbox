63771101d13d5e3a7deef5e54d6f2aa4f5d8177a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:08:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox servo estas okupata inicializar alia uzanto.';
+$lang['dropbox_app_description'] = 'Demetuja dosiero retprovizanton servo operaciita de Dropbox, Inc. kiu proponas nubo stokado, dosiera samtempigo kaj kliento programaro.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'dosierujo Grandeco';
+$lang['dropbox_missing_home_folder'] = 'Missing hejmo dosierujo - kontrolu uzanto aliro al dosiero servoj.';
+$lang['dropbox_no_data'] = 'Datumoj ...';
+$lang['dropbox_reset_complete'] = 'Lokalajn Dropbox loka dosierujo estas forigita kaj klarigi sync informo reset.';
+$lang['dropbox_reset_confirm'] = 'Ĝi rekomendas vin desvincularse ĉi aparaton el la Dropbox portalo antaŭ recomposición (forigo) lokalajn Dropbox dosierujo.';
+$lang['dropbox_reset_delete'] = 'Retrovu / Forigi Loka Dosierujo';
+$lang['dropbox_service_not_running'] = 'Dropbox servo ne funkcias.';
+$lang['dropbox_starting_service'] = 'Vi ne povos komenci la Dropbox servo ĝis almenaŭ unu uzanto agordita ilia konto. Ensalutu por Webconfig kiel uzanto (ne radiko) kaj kuras tra la Dropbox syncronization instalinstrukciojn.';
+$lang['dropbox_status_not_initialized'] = 'Pritraktata uzanto inicialización';
+$lang['dropbox_user_invalid'] = 'Dropbox uzanto estas nevalida.';
+$lang['dropbox_user_no_access'] = 'La administristo ne instalis ĉi konto kun Dropbox aliro.';
+$lang['dropbox_users'] = 'Dropbox Uzantoj';
