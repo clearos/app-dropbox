90c8dd47c3bdd8f5b8d11fc328b8de7de2ca1f64
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:09:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox szolgáltatás foglalt inicializálása egy másik felhasználó.';
+$lang['dropbox_app_description'] = 'Dropbox egy fájl hosting szolgáltatás által üzemeltetett Dropbox, Inc. kínál felhő tároló, fájlszinkronizálás és kliens szoftver.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Folder Size';
+$lang['dropbox_missing_home_folder'] = 'Hiányzó home mappa - ellenőrizze a felhasználói hozzáférést fájlszolgáltatásokat.';
+$lang['dropbox_no_data'] = 'Nincs adat...';
+$lang['dropbox_reset_complete'] = 'A helyi Dropbox helyi mappát törölték és a fiók szinkronizálási információ törlődik.';
+$lang['dropbox_reset_confirm'] = 'Javasoljuk, hogy kettéválasztja az eszközt a Dropbox portál a visszaállítás előtt (törlés) a helyi Dropbox mappában.';
+$lang['dropbox_reset_delete'] = 'Reset / törlése Helyi mappa';
+$lang['dropbox_service_not_running'] = 'Dropbox szolgáltatás nem fut.';
+$lang['dropbox_starting_service'] = 'Ön nem fogja tudni elindítani a Dropbox szolgáltatás, amíg legalább egy felhasználó beállította a saját fiókjában. Belépés a Webconfig felhasználóként (nem root) és futtasd át a Dropbox szinkronizálás beállítása.';
+$lang['dropbox_status_not_initialized'] = 'Függő felhasználói inicializálás';
+$lang['dropbox_user_invalid'] = 'Dropbox felhasználó érvénytelen.';
+$lang['dropbox_user_no_access'] = 'A rendszergazda nem állította be a fiókot Dropbox hozzáférést.';
+$lang['dropbox_users'] = 'Dropbox felhasználóknak';
