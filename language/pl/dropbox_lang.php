49126ebbe39d66b0e25a3bb7f080b68602707b21
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:14:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'usługi Dropbox jest zajęty inicjowania innego użytkownika.';
+$lang['dropbox_app_description'] = 'Dropbox to usługa hosting plików obsługiwane przez Dropbox, Inc., która oferuje przechowywanie w chmurze, synchronizację plików i oprogramowania klienckiego.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Folder Size';
+$lang['dropbox_missing_home_folder'] = 'Brakujące katalog domowy - sprawdź użytkownikowi dostęp do usług plików.';
+$lang['dropbox_no_data'] = 'Brak danych...';
+$lang['dropbox_reset_complete'] = 'Lokalny Dropbox folderu lokalnego został usunięty i synchronizacji konta resetu informacyjnego.';
+$lang['dropbox_reset_confirm'] = 'Zaleca się odłączyć to urządzenie z portalu Dropbox przed kasowaniem (kasowanie) swój lokalny folder Dropbox.';
+$lang['dropbox_reset_delete'] = 'Reset / Usuń lokalny folder';
+$lang['dropbox_service_not_running'] = 'usługi Dropbox nie jest uruchomiony.';
+$lang['dropbox_starting_service'] = 'Nie będzie w stanie uruchomić usługę Dropbox, aż przynajmniej jeden użytkownik ustawił swoje konto. Zaloguj się webconfig jako użytkownika (non-root) i prowadzony przez konfigurację synchronizację Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'Do czasu inicjalizacji użytkownik';
+$lang['dropbox_user_invalid'] = 'użytkownik Dropbox jest nieprawidłowy.';
+$lang['dropbox_user_no_access'] = 'Administrator nie skonfigurował to konto z dostępem Dropbox.';
+$lang['dropbox_users'] = 'Dropbox Użytkownicy';
