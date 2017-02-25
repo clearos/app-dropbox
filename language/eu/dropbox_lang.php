42bceef265e283664dc906727e682fb887ecd191
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:06:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox zerbitzua lanpetuta beste erabiltzaile bat hasieratzeko da.';
+$lang['dropbox_app_description'] = 'Dropbox fitxategi hosting zerbitzua Dropbox, Inc. operatu hodeia biltegiratze, fitxategia sinkronizazioa eta bezeroaren softwarea eskaintzen da.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Folder neurria';
+$lang['dropbox_missing_home_folder'] = 'Falta etxeko karpeta - egiaztatu erabiltzaile sarrera fitxategia zerbitzuetara.';
+$lang['dropbox_no_data'] = 'Daturik ez ...';
+$lang['dropbox_reset_complete'] = 'Zure tokiko Dropbox karpeta lokal ezabatu da eta kontua sync informazio berrezarri.';
+$lang['dropbox_reset_confirm'] = 'Gomendagarria da gailu hau desmarkatzen Dropbox atariko duzu berrezartzea (ezabatzea) zure tokiko Dropbox karpeta aurretik.';
+$lang['dropbox_reset_delete'] = 'Berrezarri / ezabatu Local Folder';
+$lang['dropbox_service_not_running'] = 'Dropbox zerbitzua ez dago martxan.';
+$lang['dropbox_starting_service'] = 'Ez duzu, Dropbox zerbitzua hasteko gai izango behintzat bere kontua konfiguratu duela arte. Sartu erabiltzailea (ez root) gisa Webconfig eta Dropbox sinkronizazioa setup exekutatu bidez.';
+$lang['dropbox_status_not_initialized'] = 'Erabiltzaile hasieratze oraindik';
+$lang['dropbox_user_invalid'] = 'Dropbox erabiltzaileak ez du balio.';
+$lang['dropbox_user_no_access'] = 'Administratzaileak ez du kontu hau Dropbox sarbidea duen antolaketa.';
+$lang['dropbox_users'] = 'Dropbox erabiltzaileak';
