5304f20f4e5455c07474c55cd23747e1223a3ffb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:10:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox ọrụ bụ ọrụ n&#39;aka initializing onye ọrụ ọzọ.';
+$lang['dropbox_app_description'] = 'Dropbox bụ a file mba ọrụ ejikwara site Dropbox, Inc. na awade ojii nchekwa, file mmekọrịta na ahịa software.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'nchekwa Size';
+$lang['dropbox_missing_home_folder'] = '-Efu home folder - elele onye ọrụ ohere faịlụ ọrụ.';
+$lang['dropbox_no_data'] = 'Ọ dịghị data ...';
+$lang['dropbox_reset_complete'] = 'Mpaghara gị Dropbox local folder e-ehichapụ na akaụntụ mmekọrịta ọmụma nrụpụta.';
+$lang['dropbox_reset_confirm'] = 'Ọ na-atụ aro ị ichefu nke a ngwaọrụ site na Dropbox portal tupu resetting (ihichapụ) mpaghara gị Dropbox nchekwa.';
+$lang['dropbox_reset_delete'] = 'Tọgharia / Hichapụ Obodo Folder';
+$lang['dropbox_service_not_running'] = 'Dropbox ọrụ na-adịghị na-agba ọsọ.';
+$lang['dropbox_starting_service'] = 'Ị ga-enwe ike na-amalite Dropbox ọrụ ruo mgbe ọ dịkarịa ala otu onye ọrụ ahụ ahazi n&#39;ihi ha. Nbanye Webconfig dị ka onye ọrụ (na-abụghị ndị mgbọrọgwụ) na-agba ọsọ site Dropbox syncronization ntọlite.';
+$lang['dropbox_status_not_initialized'] = 'Rue mgbe onye ọrụ initialization';
+$lang['dropbox_user_invalid'] = 'Dropbox onye ọrụ bụ ghara ịdị irè.';
+$lang['dropbox_user_no_access'] = 'The nchịkwa adịghị melite a na akaụntụ na Dropbox ohere.';
+$lang['dropbox_users'] = 'Dropbox Ọrụ';
