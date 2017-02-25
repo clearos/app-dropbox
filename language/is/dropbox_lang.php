8fe225af493553b99c0a5818e5609a45ab356551
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:10:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox þjónusta er upptekinn frumstilla annan notanda.';
+$lang['dropbox_app_description'] = 'Dropbox er skrá vefhýsingar rekin af Dropbox, Inc., sem býður upp á ský geymsla, skrá samstillingu og viðskiptavinur hugbúnaður.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Folder Size';
+$lang['dropbox_missing_home_folder'] = 'Vantar heimili mappa - athuga aðgang notanda að skrá þjónustu.';
+$lang['dropbox_no_data'] = 'Engin gögn...';
+$lang['dropbox_reset_complete'] = 'Sveitarstjórn Dropbox sveitarfélaga mappa hefur verið eytt og reikningur sync upplýsingar endurstilla.';
+$lang['dropbox_reset_confirm'] = 'Það er mælt með að þú aftengt þetta tæki frá Dropbox vefsíðunni áður endurstilla (eyða) sveitarfélaga Dropbox möppu.';
+$lang['dropbox_reset_delete'] = 'Endurstilla / Eyða heimamaður mappa';
+$lang['dropbox_service_not_running'] = 'Dropbox þjónusta er ekki í gangi.';
+$lang['dropbox_starting_service'] = 'Þú munt ekki vera fær um að hefja Dropbox þjónustu fyrr en að minnsta kosti einn notandi hefur stillt reikning sinn. Innskráning til að Webconfig sem notandi (ekki rót) og hlaupa í gegnum Dropbox syncronization skipulag.';
+$lang['dropbox_status_not_initialized'] = 'Bíður notandi frumstilling';
+$lang['dropbox_user_invalid'] = 'Dropbox notandi er ógilt.';
+$lang['dropbox_user_no_access'] = 'Stjórnandi hefur ekki sett upp þennan reikning með Dropbox aðgang.';
+$lang['dropbox_users'] = 'Dropbox notendur';
