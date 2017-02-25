8a505edfb2bff306da3afb7cf95fee43a323aa6b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:15:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox huduma ni busy initializing mtumiaji mwingine.';
+$lang['dropbox_app_description'] = 'Dropbox ni faili hosting huduma kuendeshwa na Dropbox, Inc ambayo inatoa kuhifadhi wingu, synchronization faili na programu ya mteja.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'folder Ukubwa';
+$lang['dropbox_missing_home_folder'] = 'Kukosa nyumbani folder - kuangalia user upatikanaji wa huduma file.';
+$lang['dropbox_no_data'] = 'Hakuna data ...';
+$lang['dropbox_reset_complete'] = 'Dropbox lako ndani folder umefutwa na akaunti upatanishi maelezo upya.';
+$lang['dropbox_reset_confirm'] = 'Ni ilipendekeza wewe kujitenga kifaa hiki kutoka Dropbox portal kabla ya kuweka upya (kufuta) folder lako Dropbox.';
+$lang['dropbox_reset_delete'] = 'Rudisha / Futa Folder za Mitaa';
+$lang['dropbox_service_not_running'] = 'Dropbox huduma si mbio.';
+$lang['dropbox_starting_service'] = 'Huwezi kuwa na uwezo wa kuanza huduma Dropbox mpaka user angalau moja amesanidi akaunti zao. Ingia Webconfig kama user (yasiyo ya mizizi) na kukimbia kwa njia ya Dropbox syncronization kuanzisha.';
+$lang['dropbox_status_not_initialized'] = 'Inasubiri user initialization';
+$lang['dropbox_user_invalid'] = 'Dropbox user ni batili.';
+$lang['dropbox_user_no_access'] = 'msimamizi hana kuanzisha akaunti hii na upatikanaji Dropbox.';
+$lang['dropbox_users'] = 'Watumiaji Dropbox';
