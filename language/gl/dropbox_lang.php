77ebfb92e487182341be7a953caf75a9819e5722
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:08:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'servizo Dropbox está ocupado arrincar outro usuario.';
+$lang['dropbox_app_description'] = 'Dropbox é un servizo de hospedaxe de arquivos operado polo Dropbox, Inc, que ofrece almacenamento en nube, sincronización de arquivos e software cliente.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'tamaño cartafol';
+$lang['dropbox_missing_home_folder'] = 'Falta cartafol home - comprobar o acceso dos usuarios aos servizos de arquivo.';
+$lang['dropbox_no_data'] = 'Non hai datos ...';
+$lang['dropbox_reset_complete'] = 'Seu cartafol local Dropbox local, foi borrada e conta a información de redefinición de sincronización.';
+$lang['dropbox_reset_confirm'] = 'Recoméndase que disociar dispositivo dende o portal Dropbox antes de reiniciar (eliminar) seu cartafol local Dropbox.';
+$lang['dropbox_reset_delete'] = 'Reset / Eliminar cartafol local';
+$lang['dropbox_service_not_running'] = 'servizo Dropbox non está en execución.';
+$lang['dropbox_starting_service'] = 'Non será capaz de iniciar o servizo Dropbox ata polo menos un usuario configurou a súa conta. Debes WebConfig como usuario (non desde root) e executa a configuración da sincronización Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'Mentres se agarda a inicialización do usuario';
+$lang['dropbox_user_invalid'] = 'user Dropbox é válido.';
+$lang['dropbox_user_no_access'] = 'O administrador non creou esta conta con acceso Dropbox.';
+$lang['dropbox_users'] = 'Os usuarios Dropbox';
