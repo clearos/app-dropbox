8940febfeeddc48c2090c220c6818cbae1d8d95c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:10:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'layanan Dropbox sibuk inisialisasi pengguna lain.';
+$lang['dropbox_app_description'] = 'Dropbox adalah file hosting yang dioperasikan oleh Dropbox, Inc. yang menawarkan penyimpanan awan, sinkronisasi file dan perangkat lunak klien.';
+$lang['dropbox_app_name'] = 'dropbox';
+$lang['dropbox_folder_size'] = 'Ukuran folder';
+$lang['dropbox_missing_home_folder'] = 'Hilang folder rumah - memeriksa akses pengguna ke layanan file.';
+$lang['dropbox_no_data'] = 'Tidak ada data...';
+$lang['dropbox_reset_complete'] = 'Dropbox folder lokal setempat telah dihapus dan informasi account sync ulang.';
+$lang['dropbox_reset_confirm'] = 'Dianjurkan Anda memutus perangkat ini dari portal Dropbox sebelum ulang (menghapus) folder Dropbox setempat.';
+$lang['dropbox_reset_delete'] = 'Reset / Hapus Folder Lokal';
+$lang['dropbox_service_not_running'] = 'layanan Dropbox tidak berjalan.';
+$lang['dropbox_starting_service'] = 'Anda tidak akan dapat memulai layanan Dropbox sampai setidaknya satu pengguna telah mengkonfigurasi akun mereka. Login ke webconfig sebagai pengguna (non-root) dan menjalankan melalui Dropbox sinkronisasi setup.';
+$lang['dropbox_status_not_initialized'] = 'Tertunda inisialisasi pengguna';
+$lang['dropbox_user_invalid'] = 'Dropbox pengguna tidak valid.';
+$lang['dropbox_user_no_access'] = 'administrator belum menyiapkan akun ini dengan akses Dropbox.';
+$lang['dropbox_users'] = 'Pengguna dropbox';
