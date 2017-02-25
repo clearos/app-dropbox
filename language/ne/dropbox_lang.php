e91a332d06149296366fc3cee60384fa3d56d7bb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:13:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'ड्रपबक्स सेवा अन्य प्रयोगकर्ता थालनी व्यस्त छ।';
+$lang['dropbox_app_description'] = 'ड्रपबक्स क्लाउड भण्डारण, फाइल समिकरण र ग्राहक सफ्टवेयर प्रदान ड्रपबक्स, Inc द्वारा संचालित एक होस्टिंग सेवा हो।';
+$lang['dropbox_app_name'] = 'ड्रपबक्स';
+$lang['dropbox_folder_size'] = 'फोल्डर आकार';
+$lang['dropbox_missing_home_folder'] = 'छुटेको घर फोल्डर - फाइल सेवाहरूमा प्रयोगकर्तालाई पहुँच जाँच गर्नुहोस्।';
+$lang['dropbox_no_data'] = 'डाटा छैन...';
+$lang['dropbox_reset_complete'] = 'आफ्नो स्थानीय Dropbox स्थानीय फोल्डर हटाउन खाता सिंक जानकारी रिसेट गरिएको छ।';
+$lang['dropbox_reset_confirm'] = 'यो (मेटाउने) आफ्नो स्थानीय Dropbox फोल्डर रिसेट गर्नु अघि तपाईं Dropbox पोर्टल यो उपकरण संग असम्बन्धित सिफारिस गरिएको छ।';
+$lang['dropbox_reset_delete'] = 'रिसेट गर्नुहोस् / स्थानीय फोल्डरमेट्नुहोस्';
+$lang['dropbox_service_not_running'] = 'ड्रपबक्स सेवा चलिरहेको छ।';
+$lang['dropbox_starting_service'] = 'कम्तिमा एक प्रयोगकर्ता आफ्नो खाता कन्फिगर नभएसम्म तपाईं Dropbox सेवा सुरु गर्न सक्षम हुने छैन। एक प्रयोगकर्ता (गैर-मूल) को रूपमा Webconfig गर्न र Dropbox syncronization सेटअप मार्फत चलाउन लगइन गर्नुहोस्।';
+$lang['dropbox_status_not_initialized'] = 'बाँकी रहेको प्रयोगकर्ता प्रारम्भ';
+$lang['dropbox_user_invalid'] = 'ड्रपबक्स प्रयोगकर्ता अमान्य छ।';
+$lang['dropbox_user_no_access'] = 'प्रशासक संग Dropbox पहुँच यो खाता सेट छैन अप।';
+$lang['dropbox_users'] = 'ड्रपबक्स प्रयोगकर्ता';
