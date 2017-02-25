052c24380799056e3167248f0515e600ab81e50c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:09:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'υπηρεσία Dropbox είναι απασχολημένη με την προετοιμασία άλλο χρήστη.';
+$lang['dropbox_app_description'] = 'Dropbox είναι μια υπηρεσία φιλοξενίας αρχείων λειτουργεί από το Dropbox, Inc. που προσφέρει σύννεφο αποθήκευσης, συγχρονισμού αρχείων και λογισμικό πελάτη.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Μέγεθος φακέλου';
+$lang['dropbox_missing_home_folder'] = 'Λείπει το φάκελο στο σπίτι - ελέγξτε την πρόσβαση των χρηστών σε υπηρεσίες αρχείο.';
+$lang['dropbox_no_data'] = 'Χωρίς δεδομένα...';
+$lang['dropbox_reset_complete'] = 'τοπική Dropbox τοπικό φάκελο σας έχει διαγραφεί και ο λογαριασμός συγχρονισμού επαναφορά πληροφοριών.';
+$lang['dropbox_reset_confirm'] = 'Συνιστάται να διαχωρίσετε αυτή τη συσκευή από την πύλη Dropbox πριν από την επαναφορά (διαγραφή) τοπικό φάκελο σας Dropbox.';
+$lang['dropbox_reset_delete'] = 'Επαναφορά / Διαγραφή Τοπική φακέλου';
+$lang['dropbox_service_not_running'] = 'υπηρεσία Dropbox δεν εκτελείται.';
+$lang['dropbox_starting_service'] = 'Δεν θα είστε σε θέση να ξεκινήσετε την υπηρεσία Dropbox μέχρι τουλάχιστον ένας χρήστης έχει ρυθμίσει το λογαριασμό τους. Είσοδος στο Webconfig ως χρήστης (non-root) και να τρέξει μέσα από την εγκατάσταση συγχρονισμός Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'Εν αναμονή της προετοιμασίας χρήστη';
+$lang['dropbox_user_invalid'] = 'χρήστη Dropbox δεν είναι έγκυρο.';
+$lang['dropbox_user_no_access'] = 'Ο διαχειριστής δεν έχει δημιουργήσει αυτό το λογαριασμό με πρόσβαση στο Dropbox.';
+$lang['dropbox_users'] = 'Dropbox χρήστες';
