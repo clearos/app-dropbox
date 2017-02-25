796f6a52a05de45cd5b9bf66270a398fa8f3a082
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:11:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox қызмет басқа пайдаланушыға инициализациялау бос емес.';
+$lang['dropbox_app_description'] = 'Dropbox бұлтты сақтау, файл үндестіруді және клиенттік бағдарламалық қамтамасыз ұсынады Dropbox, Inc. пайдаланатын файл хостинг қызмет болып табылады.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Folder Size';
+$lang['dropbox_missing_home_folder'] = 'Missing қапшығы - қызметтер файлына пайдаланушы қол жеткізуін тексеру.';
+$lang['dropbox_no_data'] = 'Деректер жоқ ...';
+$lang['dropbox_reset_complete'] = 'Сіздің жергілікті Dropbox жергілікті қалта жойылады және синхрондау ақпарат ысыруды есепке алынды.';
+$lang['dropbox_reset_confirm'] = 'Ол сіздің жергілікті Dropbox қалтасы (жою) қайта орнатпас бұрын, Dropbox порталынан Бұл құрылғыны ажырату операциясын ұсынылады.';
+$lang['dropbox_reset_delete'] = 'Reset / қапшықтары жою';
+$lang['dropbox_service_not_running'] = 'Dropbox қызмет іске қосылмаған.';
+$lang['dropbox_starting_service'] = 'кем дегенде бір пайдаланушы тіркелгісін теңшелген дейін Сіз Dropbox қызметіне бастау мүмкін болмайды. пайдаланушы (түбір емес) сияқты WebConfig Кіру және Dropbox желісінің математикалық моделін үндестіру орнату арқылы іске қосыңыз.';
+$lang['dropbox_status_not_initialized'] = 'Күтудегі пайдаланушы инициализации';
+$lang['dropbox_user_invalid'] = 'Dropbox пайдаланушы жарамсыз болып табылады.';
+$lang['dropbox_user_no_access'] = 'Әкімші Dropbox қол жеткізу осы есептік жазбасын орнату жоқ.';
+$lang['dropbox_users'] = 'Dropbox Пайдаланушылар';
