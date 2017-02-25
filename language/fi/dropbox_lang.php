87902c695e4e047e71ae518ee4efbc844f9d2425
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:08:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox palvelu on varattu alustamista toiselle käyttäjälle.';
+$lang['dropbox_app_description'] = 'Dropbox on verkkotallennustila liikennöi Dropbox, Inc., joka tarjoaa pilvi varastointi, tiedostojen synkronointi ja client-ohjelmisto.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Folder Size';
+$lang['dropbox_missing_home_folder'] = 'Puuttuvat kotikansiossasi - tarkista käyttäjän pääsyä tiedostoon palveluja.';
+$lang['dropbox_no_data'] = 'Ei dataa...';
+$lang['dropbox_reset_complete'] = 'Paikallinen Dropbox paikallinen kansio on poistettu tilin synkronoida tiedot nollataan.';
+$lang['dropbox_reset_confirm'] = 'On suositeltavaa erottaa tämän laitteen Dropbox portaalista ennen nollauksen (poistetaan) paikalliseen Dropbox-kansioon.';
+$lang['dropbox_reset_delete'] = 'Nollaa / Poista paikallinen kansio';
+$lang['dropbox_service_not_running'] = 'Dropbox palvelu ei ole käynnissä.';
+$lang['dropbox_starting_service'] = 'Et voi aloittaa Dropbox palvelu ennen kuin vähintään yksi käyttäjä on määrittänyt tilinsä. Kirjautua Webconfig käyttäjänä (ei root) ja kulkevat Dropbox synkronointi asetukset.';
+$lang['dropbox_status_not_initialized'] = 'Odottaa käyttäjän alustuksen';
+$lang['dropbox_user_invalid'] = 'Dropbox käyttäjä ei kelpaa.';
+$lang['dropbox_user_no_access'] = 'Ylläpitäjä ei ole perustanut tätä tilin Dropbox pääsy.';
+$lang['dropbox_users'] = 'Dropbox käyttäjät';
