8c607d4379a2135d904eb375bc1ff8807118bd30
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:07:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox msonkhano wotanganidwa potsegula wosuta wina.';
+$lang['dropbox_app_description'] = 'Dropbox ndi file kuchititsa msonkhano ntchito ndi Dropbox, Inc. kuti amapereka yosungirako mtambo, kalunzanitsidwe file ndi mapulogalamu kasitomala.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'foda Kukula';
+$lang['dropbox_missing_home_folder'] = 'Kusowa kwawo foda - onani mwayi wogwiritsa misonkhano file.';
+$lang['dropbox_no_data'] = 'No deta ...';
+$lang['dropbox_reset_complete'] = 'Dropbox kwanuko kumeneko foda kolumikizana ndi mlandu kulunzanitsa mudziwe Yambitsaninso.';
+$lang['dropbox_reset_confirm'] = 'Ndi bwino inu kusayanjananawo chipangizo ichi kwa Dropbox zipata pamaso resetting (osonyeza) foda kwanuko Dropbox.';
+$lang['dropbox_reset_delete'] = 'Bwezerani / Chotsani Foda Local';
+$lang['dropbox_service_not_running'] = 'Dropbox utumiki osati akuthamanga.';
+$lang['dropbox_starting_service'] = 'Inu sangathe kuyamba utumiki Dropbox mpaka wosuta pafupifupi munthu kukhazikitsidwa nkhani zawo. Kulowa kwa Webconfig monga wosuta ndi (sanali mizu) ndi kupyola mu Dropbox syncronization khwekhwe.';
+$lang['dropbox_status_not_initialized'] = 'Podikira wosuta initialization';
+$lang['dropbox_user_invalid'] = 'Dropbox wosuta ndi opanda pake.';
+$lang['dropbox_user_no_access'] = 'Mkuluyu wakhala sipakhala nkhaniyi ndi kupeza Dropbox.';
+$lang['dropbox_users'] = 'Ogwiritsa Dropbox';
