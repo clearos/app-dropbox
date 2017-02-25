60e3769725d888d27004a32930a54e82b9516aad
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:13:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'perkhidmatan Dropbox sibuk Memulakan pengguna lain.';
+$lang['dropbox_app_description'] = 'Dropbox adalah perkhidmatan fail hosting dikendalikan oleh Dropbox, Inc yang menawarkan penyimpanan awan, penyegerakan fail dan perisian pelanggan.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Saiz folder';
+$lang['dropbox_missing_home_folder'] = 'Hilang folder home - menyemak akses pengguna kepada perkhidmatan fail.';
+$lang['dropbox_no_data'] = 'Tiada data...';
+$lang['dropbox_reset_complete'] = 'folder tempatan Dropbox tempatan anda dihapuskan dan akaun set semula maklumat penyegerakan.';
+$lang['dropbox_reset_confirm'] = 'Adalah disyorkan anda memisahkan peranti ini daripada portal Dropbox sebelum menetapkan semula (memotong) folder Dropbox tempatan anda.';
+$lang['dropbox_reset_delete'] = 'Reset / Padam Folder Tempatan';
+$lang['dropbox_service_not_running'] = 'perkhidmatan Dropbox tidak berjalan.';
+$lang['dropbox_starting_service'] = 'Anda tidak akan dapat untuk memulakan perkhidmatan Dropbox sehingga sekurang-kurangnya satu pengguna telah dikonfigurasikan akaun mereka. Log masuk untuk Webconfig sebagai pengguna (tidak root) dan berjalan melalui persediaan syncronization Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'Sementara menunggu pengawalan pengguna';
+$lang['dropbox_user_invalid'] = 'Dropbox pengguna tidak sah.';
+$lang['dropbox_user_no_access'] = 'pentadbir tidak menyediakan akaun ini dengan akses Dropbox.';
+$lang['dropbox_users'] = 'Pengguna Dropbox';
