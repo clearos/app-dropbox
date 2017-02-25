fb0dcd3b1ebb67a71199dc324a5df42027d22afb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:15:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'adeegga DropBox waa mashquul bilaabida user kale.';
+$lang['dropbox_app_description'] = 'DropBox waa adeeg file marti ah oo ay DropBox, Inc. oo bixiya daruur kaydinta, .Wadashaqayntaas file iyo software macmiilka.';
+$lang['dropbox_app_name'] = 'DropBox';
+$lang['dropbox_folder_size'] = 'Size folder';
+$lang['dropbox_missing_home_folder'] = 'La&#39;yahay folder guriga - hubiyo helitaanka user adeegyada file.';
+$lang['dropbox_no_data'] = 'No data ...';
+$lang['dropbox_reset_complete'] = 'Your DropBox folder maxaliga ah ee maxaliga ah ayaa la tirtiray iyo macluumaad ku saabsan koontada keddibna u hagaagsan.';
+$lang['dropbox_reset_confirm'] = 'Waxaa lagu talinayaa in aad ka portal DropBox ka fogeeyey qalab this ka hor resetting (tirka) aad folder DropBox maxaliga ah.';
+$lang['dropbox_reset_delete'] = 'Dib u celi / Delete Folder Local';
+$lang['dropbox_service_not_running'] = 'adeegga DropBox aan la ordaya.';
+$lang['dropbox_starting_service'] = 'ma awoodi doontid inaad si ay u bilaabaan adeeg DropBox ilaa ugu yaraan hal user ayaa qaybiyay ay account. Soo gal Webconfig sida user ah (non-xidid) iyo dhex Mudanayaasha syncronization DropBox.';
+$lang['dropbox_status_not_initialized'] = 'Inta initialization user';
+$lang['dropbox_user_invalid'] = 'DropBox user waa baadil.';
+$lang['dropbox_user_no_access'] = 'maamulaha ayaa weli qotomin account this la heli DropBox.';
+$lang['dropbox_users'] = 'Users DropBox';
