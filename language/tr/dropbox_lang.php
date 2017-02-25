e48a9ece502a4748afeb2fa204c8c47efd35d459
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:16:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox hizmeti başka bir kullanıcı başlatılıyor meşgul.';
+$lang['dropbox_app_description'] = 'Dropbox bulut depolama, dosya senkronizasyonu ve istemci yazılımı sunuyor Dropbox, Inc. tarafından işletilen bir dosya barındırma hizmetidir.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'klasör Boyutu';
+$lang['dropbox_missing_home_folder'] = 'giriş klasörü Eksik - dosya hizmetlerine kullanıcı erişimini kontrol edin.';
+$lang['dropbox_no_data'] = 'Veri yok...';
+$lang['dropbox_reset_complete'] = 'Yerel Dropbox yerel klasör silinmiş ve senkronizasyon bilgi sıfırlama hesap edilmiştir.';
+$lang['dropbox_reset_confirm'] = '(Silme) yerel Dropbox klasörüne sıfırlamadan önce Dropbox portal Bu cihaz ilişkisini tavsiye edilir.';
+$lang['dropbox_reset_delete'] = '/ Reset Yerel Klasör Silme';
+$lang['dropbox_service_not_running'] = 'Dropbox hizmeti çalışmıyor.';
+$lang['dropbox_starting_service'] = 'en az bir kullanıcı hesabını yapılandırılmış kadar Dropbox hizmeti başlatmak için mümkün olmayacaktır. Bir kullanıcı (non-root) olarak webConfig ve Dropbox senkronizasyon kurulumu aracılığıyla çalıştırmak için giriş yapın.';
+$lang['dropbox_status_not_initialized'] = 'Bekleyen kullanıcı başlatma';
+$lang['dropbox_user_invalid'] = 'Dropbox kullanıcı geçersiz.';
+$lang['dropbox_user_no_access'] = 'yönetici Dropbox erişimi olan bu hesabı kurmamıştır.';
+$lang['dropbox_users'] = 'dropbox Kullanıcılar';
