ff48910aafc33b8e779ba7871b7333b1ffc387d3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:13:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'سرویس Dropbox مشغول مقدار دهی اولیه یک کاربر دیگر است.';
+$lang['dropbox_app_description'] = 'Dropbox به یک سرویس میزبانی فایل های Dropbox به، شرکت ارائه می دهد که عمل ذخیره سازی ابر، هماهنگ سازی فایل و نرم افزار مشتری است.';
+$lang['dropbox_app_name'] = 'Dropbox به';
+$lang['dropbox_folder_size'] = 'حجم پوشه';
+$lang['dropbox_missing_home_folder'] = 'گمشده پوشه خانه - بررسی دسترسی کاربر را به سرویس های فایل.';
+$lang['dropbox_no_data'] = 'اطلاعاتی وجود ندارد...';
+$lang['dropbox_reset_complete'] = 'Dropbox به پوشه های محلی محلی شما حذف شده است و حساب تنظیم مجدد اطلاعات همگام.';
+$lang['dropbox_reset_confirm'] = 'توصیه می شود که این دستگاه را جدا از پورتال Dropbox به قبل از بازنشانی (حذف) پوشه Dropbox محلی خود را.';
+$lang['dropbox_reset_delete'] = 'تنظیم مجدد / حذف پوشه های محلی';
+$lang['dropbox_service_not_running'] = 'سرویس Dropbox به در حال اجرا نیست.';
+$lang['dropbox_starting_service'] = 'شما نمی قادر خواهد بود برای شروع سرویس Dropbox تا حداقل یک کاربر حساب خود پیکربندی کرده است. ورود به عنوان یک کاربر (غیر ریشه) Webconfig و اجرا از طریق راه اندازی همگامسازی Dropbox به است.';
+$lang['dropbox_status_not_initialized'] = 'در انتظار اولیه کاربران';
+$lang['dropbox_user_invalid'] = 'کاربر Dropbox نامعتبر است.';
+$lang['dropbox_user_no_access'] = 'مدیریت سایت تنظیم نشده این حساب کاربری با دسترسی Dropbox به است.';
+$lang['dropbox_users'] = 'کاربران Dropbox';
