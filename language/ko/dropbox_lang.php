326957414f0d133add814da2e4b412fcab71c243
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:11:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = '보관 서비스는 다른 사용자를 초기화하는 중입니다.';
+$lang['dropbox_app_description'] = '드롭 박스는 클라우드 스토리지, 파일 동기화 및 클라이언트 소프트웨어를 제공 드롭 박스, Inc.가 운영하는 파일 호스팅 서비스입니다.';
+$lang['dropbox_app_name'] = '드롭 박스';
+$lang['dropbox_folder_size'] = '폴더 크기';
+$lang['dropbox_missing_home_folder'] = '홈 폴더를 누락 - 파일 서비스에 대한 사용자 액세스를 확인합니다.';
+$lang['dropbox_no_data'] = '데이터가 없습니다 ...';
+$lang['dropbox_reset_complete'] = '지역 드롭 박스 로컬 폴더 삭제 및 동기화 정보 재설정을 고려하고있다.';
+$lang['dropbox_reset_confirm'] = '(삭제) 해당 지역의 드롭 박스 폴더를 재설정하기 전에 보관 포털에서이 장치를 해제하는 것이 좋습니다.';
+$lang['dropbox_reset_delete'] = '/ 리셋 로컬 폴더를 삭제';
+$lang['dropbox_service_not_running'] = '보관 서비스가 실행되고 있지 않습니다.';
+$lang['dropbox_starting_service'] = '적어도 하나의 사용자가 자신의 계정을 구성 할 때까지 당신은 드롭 박스 서비스를 시작할 수 없습니다. 사용자 (비 루트)로 Webconfig 및 보관 용 동기화 해 설치를 통해 실행하는 로그인합니다.';
+$lang['dropbox_status_not_initialized'] = '등록되지 않은 사용자 초기화';
+$lang['dropbox_user_invalid'] = '드롭 박스 사용자가 잘못되었습니다.';
+$lang['dropbox_user_no_access'] = '관리자는 보관 접속이 계정을 설정하지 않았습니다.';
+$lang['dropbox_users'] = '드롭 박스 사용자';
