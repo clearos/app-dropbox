66d4fffa78d83fe18986367d104bbab806b81dc6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:08:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox teenus on hõivatud initializing teisele kasutajale.';
+$lang['dropbox_app_description'] = 'Dropbox on fail hosting teenust haldab Dropbox, Inc., mis pakub pilve ladustamise, failide sünkroniseerimine ja kliendi tarkvara.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'kausta suurus';
+$lang['dropbox_missing_home_folder'] = 'Kadunud kodukataloogi - vaadake kasutaja juurdepääsu faili teenuseid.';
+$lang['dropbox_no_data'] = 'Andmed ei ole ...';
+$lang['dropbox_reset_complete'] = 'Teie kohalik Dropbox kohalikus kaustas on kustutatud ja moodustavad sünkroonimisandmed nullida.';
+$lang['dropbox_reset_confirm'] = 'Soovitav on sul lahti siduda see seade Dropbox portaali nullida (kustutamine) kohaliku Dropbox kausta.';
+$lang['dropbox_reset_delete'] = 'Reset / Kustuta Local Folder';
+$lang['dropbox_service_not_running'] = 'Dropbox teenus ei tööta.';
+$lang['dropbox_starting_service'] = 'Sa ei saa alustada Dropbox teenust kuni vähemalt üks kasutaja on konfigureeritud oma kontole. Sisene Webconfig kasutajana (mitte-root) ja läbib Dropbox sünkroonimise seadistamine.';
+$lang['dropbox_status_not_initialized'] = 'Kuni kasutaja initsialiseerimise';
+$lang['dropbox_user_invalid'] = 'Dropbox kasutaja on kehtetu.';
+$lang['dropbox_user_no_access'] = 'Administraator ei ole loodud selle konto Dropbox juurdepääs.';
+$lang['dropbox_users'] = 'Dropbox Kasutajad';
