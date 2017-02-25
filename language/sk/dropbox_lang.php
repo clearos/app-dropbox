ca5b31cb56453579b7662f17f4ac7260e16dbb06
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:15:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox služba je zaneprázdnený inicializuje iný užívateľ.';
+$lang['dropbox_app_description'] = 'Dropbox je súbor hostingové služby prevádzkované Dropbox, Inc., ktorá ponúka cloudové úložisko, synchronizáciu súborov a klientsky softvér.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Folder Size';
+$lang['dropbox_missing_home_folder'] = 'Chýbajúce domovskú zložku - kontrolovať prístup užívateľov k službám súborov.';
+$lang['dropbox_no_data'] = 'Žiadne dáta...';
+$lang['dropbox_reset_complete'] = 'Váš miestny Dropbox miestna zložka bola zmazaná a tvoria synchronizačné informácie Reset.';
+$lang['dropbox_reset_confirm'] = 'Odporúča odlúčiť toto zariadenie z portálu Dropbox pred obnovením (mazanie) miestne zložky Dropbox.';
+$lang['dropbox_reset_delete'] = 'Resetovať / Odstrániť Miestny priečinok';
+$lang['dropbox_service_not_running'] = 'Dropbox služba nie je spustená.';
+$lang['dropbox_starting_service'] = 'Nebudete môcť spustiť službu Dropbox, kým aspoň jeden používateľ nakonfiguroval svoj účet. Prihlásenie do webconfigov ako užívateľa (non-root) a prejsť nastavenie Dropbox syncronization.';
+$lang['dropbox_status_not_initialized'] = 'Čaká inicializácia užívateľ';
+$lang['dropbox_user_invalid'] = 'Užívateľ Dropbox je neplatný.';
+$lang['dropbox_user_no_access'] = 'Správca nenastavil tohto účtu s prístupom Dropbox.';
+$lang['dropbox_users'] = 'Dropbox Užívatelia';
