7289548391bd33be2cec4397c1c824ca634a556e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:13:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox ਸੇਵਾ ਕਿਸੇ ਹੋਰ ਨੂੰ ਯੂਜ਼ਰ ਸ਼ੁਰੂ ਰੁੱਝਿਆ ਹੋਇਆ ਹੈ.';
+$lang['dropbox_app_description'] = 'Dropbox ਇੱਕ ਫਾਇਲ ਹੋਸਟਿੰਗ ਸੇਵਾ ਡ੍ਰੌਪਬਾਕਸ, Inc. ਦੁਆਰਾ ਚਲਾਇਆ ਹੈ, ਜੋ ਕਿ ਬੱਦਲ ਸਟੋਰੇਜ਼, ਫਾਇਲ ਸਮਕਾਲੀ ਅਤੇ ਕਲਾਇਟ ਸਾਫਟਵੇਅਰ ਦੀ ਪੇਸ਼ਕਸ਼ ਕਰਦਾ ਹੈ.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'ਫੋਲਡਰ ਸਾਈਜ਼';
+$lang['dropbox_missing_home_folder'] = 'ਘਰ ਫੋਲਡਰ ਗੁੰਮ - ਫਾਇਲ ਸੇਵਾ ਕਰਨ ਲਈ ਯੂਜ਼ਰ ਪਹੁੰਚ ਚੈੱਕ ਕਰੋ.';
+$lang['dropbox_no_data'] = 'ਕੋਈ ਡਾਟਾ ...';
+$lang['dropbox_reset_complete'] = 'ਤੁਹਾਡੀ ਸਥਾਨਕ ਡ੍ਰੌਪਬਾਕਸ ਸਥਾਨਕ ਫੋਲਡਰ ਨੂੰ ਹਟਾ ਦਿੱਤਾ ਗਿਆ ਹੈ ਅਤੇ ਖਾਤਾ ਸਿੰਕ ਜਾਣਕਾਰੀ ਰੀਸੈਟ.';
+$lang['dropbox_reset_confirm'] = 'ਇਹ ਸਿਫਾਰਸ਼ ਕੀਤੀ ਹੈ (ਹਟਾਉਣ) ਆਪਣੇ ਸਥਾਨਕ ਡ੍ਰੌਪਬਾਕਸ ਫੋਲਡਰ ਨੂੰ ਰੀਸੈੱਟ ਅੱਗੇ ਤੁਹਾਨੂੰ Dropbox ਪੋਰਟਲ ਤੱਕ ਇਸ ਜੰਤਰ ਨੂੰ ਲੈਣਾ.';
+$lang['dropbox_reset_delete'] = 'ਰੀਸੈੱਟ / ਸਥਾਨਕ ਫੋਲਡਰ ਹਟਾਓ';
+$lang['dropbox_service_not_running'] = 'Dropbox ਸੇਵਾ ਨਾ ਚੱਲ ਰਿਹਾ ਹੈ.';
+$lang['dropbox_starting_service'] = 'ਤੁਹਾਨੂੰ Dropbox ਸੇਵਾ ਸ਼ੁਰੂ ਕਰਨ ਦੇ ਯੋਗ ਹੈ, ਜਦ ਤੱਕ ਘੱਟੋ-ਘੱਟ ਇੱਕ ਯੂਜ਼ਰ ਨੂੰ ਆਪਣੇ ਖਾਤੇ ਸੰਰਚਿਤ ਕੀਤਾ ਹੈ, ਨਾ ਹੋਵੇਗਾ. ਇੱਕ ਉਪਭੋਗੀ (ਗੈਰ-ਰੂਟ) ਦੇ ਤੌਰ &#39;Webconfig ਅਤੇ Dropbox syncronization ਸੈੱਟਅੱਪ ਦੁਆਰਾ ਚਲਾਉਣ ਲਈ ਲਾਗਇਨ.';
+$lang['dropbox_status_not_initialized'] = 'ਬਕਾਇਆ ਯੂਜ਼ਰ ਨੂੰ ਸ਼ੁਰੂਆਤੀ';
+$lang['dropbox_user_invalid'] = 'Dropbox ਯੂਜ਼ਰ ਨੂੰ ਗਲਤ ਹੈ.';
+$lang['dropbox_user_no_access'] = 'ਪਰਸ਼ਾਸ਼ਕ ਡ੍ਰੌਪਬਾਕਸ ਪਹੁੰਚ ਨਾਲ ਇਸ ਖਾਤੇ ਨੂੰ ਸਥਾਪਤ ਕੀਤੀ, ਨਾ ਹੈ.';
+$lang['dropbox_users'] = 'Dropbox ਉਪਭੋਗੀ';
