c471b661ac2d964323e95a202faf79a571ee17db
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:15:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'service Dropbox nyaéta riweuh initializing pamaké séjén.';
+$lang['dropbox_app_description'] = 'Dropbox nyaeta file hosting service dioperasikeun ku Dropbox, Inc. nu nawarkeun gudang awan, sinkronisasi file jeung software klien.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Ukuran polder';
+$lang['dropbox_missing_home_folder'] = 'Leungit polder imah - mariksa aksés pamaké ka ladenan file.';
+$lang['dropbox_no_data'] = 'No data ...';
+$lang['dropbox_reset_complete'] = 'Dropbox polder lokal lokal Anjeun geus dihapus jeung akun informasi singkronisasi reset.';
+$lang['dropbox_reset_confirm'] = 'Hal ieu disarankeun Anjeun peupeus alat ieu ti portal Dropbox saméméh ngareset (mupus) polder Dropbox lokal Anjeun.';
+$lang['dropbox_reset_delete'] = 'Reset / Pupus Polder Lokal';
+$lang['dropbox_service_not_running'] = 'service Dropbox teu ngajalankeun.';
+$lang['dropbox_starting_service'] = 'Anjeun moal bisa pikeun ngamimitian ladenan Dropbox nepi ka sahanteuna hiji pamaké geus ngonpigurasi akun maranéhanana. Login ka Webconfig salaku pamaké (non-akar) jeung ngajalankeun ngaliwatan Dropbox syncronization setelan.';
+$lang['dropbox_status_not_initialized'] = 'Pending initialization pamaké';
+$lang['dropbox_user_invalid'] = 'Dropbox pamaké anu sah.';
+$lang['dropbox_user_no_access'] = 'administrator geus teu nyetel akun ieu jeung aksés Dropbox.';
+$lang['dropbox_users'] = 'Pamaké Dropbox';
