64151464d81a9ba8a19ee8f23dd9f607f52119a9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:12:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox үйлчилгээ завгүй өөр нэг хэрэглэгч эхлүүлж байна.';
+$lang['dropbox_app_description'] = 'Dropbox үүл хадгалах, файл синхрончлол болон клиент програм хангамжийг санал Dropbox, Inc-ийн ажиллуулдаг файл хостинг үйлчилгээ юм.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Folder Хэмжээ';
+$lang['dropbox_missing_home_folder'] = 'Сураггүй гэр хавтсыг - файл үйлчилгээний хэрэглэгчийн хандалтыг шалгах хэрэгтэй.';
+$lang['dropbox_no_data'] = 'Өгөгдөл алга...';
+$lang['dropbox_reset_complete'] = 'Таны орон нутгийн Dropbox, орон нутгийн хавтас устгах болон синхрон мэдээлэл дахин эзэлж байна.';
+$lang['dropbox_reset_confirm'] = 'Энэ нь (устгах) нь таны локал Dropbox хавтас шинэчлэн өмнө Dropbox портал энэ төхөөрөмжийг салгах санал болгож байна.';
+$lang['dropbox_reset_delete'] = '/ Reset Орон нутгийн Folder устгах';
+$lang['dropbox_service_not_running'] = 'Dropbox үйлчилгээ ажиллаж байна.';
+$lang['dropbox_starting_service'] = 'наад зах нь нэг хэрэглэгч өөрийн данс тохируулсан хүртэл Та Dropbox үйлчилгээг эхлүүлэх боломжтой байх болно. Хэрэглэгчийн (төрийн бус эх) гэж Webconfig болон Dropbox syncronization тохиргоон дамжуулан ажиллуулах холбогдон орно уу.';
+$lang['dropbox_status_not_initialized'] = 'Хүлээгдэж буй хэрэглэгч нь эхлүүлэх';
+$lang['dropbox_user_invalid'] = 'Dropbox хэрэглэгч хүчин төгөлдөр бус байна.';
+$lang['dropbox_user_no_access'] = 'администратор Dropbox хандах энэ данс тохируулж байна.';
+$lang['dropbox_users'] = 'Dropbox-ын гишүүд';
