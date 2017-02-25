f4961a2bc6ac25ab3a625f2a50d8d3b9073ad6a3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:10:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropboxのサービスは、別のユーザーを初期化中です。';
+$lang['dropbox_app_description'] = 'Dropboxがクラウドストレージ、ファイルの同期およびクライアントソフトウェアを提供していますのDropbox、Inc.によって運営オンラインストレージです。';
+$lang['dropbox_app_name'] = 'ドロップボックス';
+$lang['dropbox_folder_size'] = 'フォルダのサイズ';
+$lang['dropbox_missing_home_folder'] = 'ホームフォルダがありません - ファイルサービスへのユーザーアクセスを確認してください。';
+$lang['dropbox_no_data'] = 'データなし...';
+$lang['dropbox_reset_complete'] = 'あなたのローカルのDropboxローカルフォルダが削除され、同期情報のリセットを占めてきました。';
+$lang['dropbox_reset_confirm'] = '（削除）、ローカルのDropboxフォルダをリセットする前に、あなたがDropboxのポータルから、このデバイスの関連付けを解除をお勧めします。';
+$lang['dropbox_reset_delete'] = 'リセット/ローカルフォルダを削除します。';
+$lang['dropbox_service_not_running'] = 'Dropboxのサービスが実行されていません。';
+$lang['dropbox_starting_service'] = '少なくとも1つのユーザが自分のアカウントを設定しているまでは、Dropboxのサービスを開始することができません。 ユーザ（非root）としてのWebConfigとのDropboxのsyncronizationのセットアップを介して実行するには、ログインしてください。';
+$lang['dropbox_status_not_initialized'] = '保留中のユーザーの初期化';
+$lang['dropbox_user_invalid'] = 'Dropboxのユーザーが無効です。';
+$lang['dropbox_user_no_access'] = '管理者は、Dropboxのアクセスでこのアカウントを設定していません。';
+$lang['dropbox_users'] = 'Dropboxのユーザ';
