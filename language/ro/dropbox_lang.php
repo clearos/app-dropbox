97a5b6e260b93847e4b855258b8069f883ca2cb1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:14:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Serviciul Dropbox este ocupat inițializează un alt utilizator.';
+$lang['dropbox_app_description'] = 'Dropbox este un serviciu ce gazduieste fișier operat de Dropbox, Inc, care oferă nor de stocare, sincronizare de fișiere și software-ul client.';
+$lang['dropbox_app_name'] = 'dropbox';
+$lang['dropbox_folder_size'] = 'Dimensiune dosar';
+$lang['dropbox_missing_home_folder'] = 'Lipsă dosar de origine - verifica accesul utilizatorilor la serviciile de fișiere.';
+$lang['dropbox_no_data'] = 'Nu există date...';
+$lang['dropbox_reset_complete'] = 'dosarul dvs. local Dropbox locală a fost ștearsă și contul de resetare a informațiilor de sincronizare.';
+$lang['dropbox_reset_confirm'] = 'Este recomandat să disociați acest dispozitiv de la portalul Dropbox înainte de a reseta (ștergere) folderul locale Dropbox.';
+$lang['dropbox_reset_delete'] = 'Resetare / Ștergere director local';
+$lang['dropbox_service_not_running'] = 'Serviciul Dropbox nu se execută.';
+$lang['dropbox_starting_service'] = 'Tu nu va fi capabil să înceapă serviciul Dropbox până când cel puțin un utilizator a configurat contul. Conectare la Webconfig ca un utilizator (non-root) și a alerga prin configurarea Dropbox sincronizarea.';
+$lang['dropbox_status_not_initialized'] = 'În așteptare de inițializare utilizator';
+$lang['dropbox_user_invalid'] = 'utilizator Dropbox este nevalid.';
+$lang['dropbox_user_no_access'] = 'Administratorul nu a configurat acest cont cu acces la Dropbox.';
+$lang['dropbox_users'] = 'Utilizatorii Dropbox';
