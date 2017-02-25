be314ce4157be5ba6a6880ed4f1474a8d75cf80d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:17:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox служба зайнята ініціалізації іншого користувача.';
+$lang['dropbox_app_description'] = 'Dropbox це сервіс файлового хостингу управляється Dropbox, Inc., яка пропонує хмара зберігання, синхронізації файлів і клієнтське програмне забезпечення.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Розмір папки';
+$lang['dropbox_missing_home_folder'] = 'Відсутня домашня папка - перевірка доступу користувачів до служб файлів.';
+$lang['dropbox_no_data'] = 'Немає даних...';
+$lang['dropbox_reset_complete'] = 'Ваша місцева локальна папка Dropbox був вилучений і рахунок синхронізації скидання інформації.';
+$lang['dropbox_reset_confirm'] = 'Рекомендується, щоб Ви роз&#39;єднати цей пристрій з порталу Dropbox перед скиданням (видалення) локальної папці Dropbox.';
+$lang['dropbox_reset_delete'] = 'Скидання / Видалити локальну папку';
+$lang['dropbox_service_not_running'] = 'Dropbox служба не запущена.';
+$lang['dropbox_starting_service'] = 'Ви не зможете запустити службу Dropbox, поки принаймні один користувач не налаштував свій аккаунт. Вхід в WebConfig як користувач (некореневим) і запустити через установку Syncronization Dropbox.';
+$lang['dropbox_status_not_initialized'] = 'До ініціалізації користувача';
+$lang['dropbox_user_invalid'] = 'користувач Dropbox є недійсним.';
+$lang['dropbox_user_no_access'] = 'Або адміністратор не встановив цей рахунок з доступом в Dropbox.';
+$lang['dropbox_users'] = 'Dropbox Користувачі';
