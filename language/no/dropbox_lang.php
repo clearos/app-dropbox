559357222fa28069b5ded9956ffb1008ad787686
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:13:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox-tjenesten er opptatt initialisering en annen bruker.';
+$lang['dropbox_app_description'] = 'Dropbox er en fil hosting tjeneste som drives av Dropbox, Inc. som tilbyr lagring i nettskyen, filsynkronisering og klientprogramvare.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Folder Size';
+$lang['dropbox_missing_home_folder'] = 'Manglende Hjem-mappen - sjekk brukertilgang til filen tjenester.';
+$lang['dropbox_no_data'] = 'Ingen data...';
+$lang['dropbox_reset_complete'] = 'Din lokale Dropbox lokal mappe har blitt slettet og konto sync informasjon reset.';
+$lang['dropbox_reset_confirm'] = 'Det anbefales at du angre denne enheten fra Dropbox portalen før tilbakestilling (slette) din lokale Dropbox-mappen.';
+$lang['dropbox_reset_delete'] = 'Tilbakestill / Slett Local Folder';
+$lang['dropbox_service_not_running'] = 'Dropbox kjører ikke.';
+$lang['dropbox_starting_service'] = 'Du vil ikke være i stand til å starte Dropbox tjenesten før minst én bruker har konfigurert kontoen sin. Logg inn for å webConfig som en bruker (ikke root) og kjøre gjennom Dropbox synkronisering oppsett.';
+$lang['dropbox_status_not_initialized'] = 'Avventer bruker initialisering';
+$lang['dropbox_user_invalid'] = 'Dropbox bruker er ugyldig.';
+$lang['dropbox_user_no_access'] = 'Administratoren har ikke satt opp denne kontoen med tilgang til Dropbox.';
+$lang['dropbox_users'] = 'Dropbox brukere';
