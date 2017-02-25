7884c74a344c7d372d3b126679542e6fe9726b31
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:17:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox xizmati boshqa foydalanuvchi boshlanmoqda band.';
+$lang['dropbox_app_description'] = 'Dropbox bulut saqlash, fayl, hamohang va mijoz dasturini taklif etadi Dropbox, Inc. tomonidan foydalaniladigan fayl hosting xizmati.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'Folder Size';
+$lang['dropbox_missing_home_folder'] = 'Bosh sahifa papkani to&#39;liqsiz - fayl xizmatlar foydalanuvchi uchun ruxsatdan tekshiring.';
+$lang['dropbox_no_data'] = 'Ma&#39;lumot yo&#39;q ...';
+$lang['dropbox_reset_complete'] = 'Mahalliy Dropbox mahalliy papka o&#39;chiriladi va sinxronlash ma&#39;lumot reset hisob qilindi.';
+$lang['dropbox_reset_confirm'] = 'Bu (yo&#39;q qilish) mahalliy Dropbox papkani tiklashdan oldin siz Dropbox portali bu qurilmani voz kechgan tavsiya etiladi.';
+$lang['dropbox_reset_delete'] = '/ Reset Mahalliy jildini o&#39;chirish';
+$lang['dropbox_service_not_running'] = 'Dropbox xizmati harakat emas.';
+$lang['dropbox_starting_service'] = 'kamida bir foydalanuvchi o&#39;z hisobini tuzilgan qadar siz Dropbox xizmatini boshlash uchun olmaysiz. Agar foydalanuvchi (non-root) sifatida webConfig va Dropbox sinxronlashtirish o&#39;rnatish orqali Kirish.';
+$lang['dropbox_status_not_initialized'] = 'Navbatdagi Foydalanuvchi boshlash';
+$lang['dropbox_user_invalid'] = 'Dropbox foydalanuvchi haqiqiy emas.';
+$lang['dropbox_user_no_access'] = 'administrator Dropbox kirish bilan ushbu hisobni tashkil yo&#39;q.';
+$lang['dropbox_users'] = 'Dropbox foydalanuvchilar';
