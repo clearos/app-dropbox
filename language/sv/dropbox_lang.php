ed6c4a3bfb3ea91ebef28d2c74c4e49683343cc0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:16:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox tjänsten är upptagen initierar en annan användare.';
+$lang['dropbox_app_description'] = 'Dropbox är en fil webbhotell som drivs av Dropbox, Inc. som erbjuder molnlagring, filsynkronisering och klientprogramvara.';
+$lang['dropbox_app_name'] = 'Dropbox';
+$lang['dropbox_folder_size'] = 'folder Size';
+$lang['dropbox_missing_home_folder'] = 'Saknade hemmapp - kontrollera användarnas åtkomst till filtjänster.';
+$lang['dropbox_no_data'] = 'Inga data...';
+$lang['dropbox_reset_complete'] = 'Din lokala Dropbox lokal mapp har tagits bort och kontosynkroniseringsinformation återställning.';
+$lang['dropbox_reset_confirm'] = 'Det rekommenderas att du tar avstånd enheten från Dropbox portalen innan återställning (raderar) din lokala mapp Dropbox.';
+$lang['dropbox_reset_delete'] = 'Återställ / radera Lokal mapp';
+$lang['dropbox_service_not_running'] = 'Dropbox tjänsten inte körs.';
+$lang['dropbox_starting_service'] = 'Du kommer inte att kunna starta Dropbox bruk förrän åtminstone en användare har konfigurerat sitt konto. Logga in för att Webconfig som användare (icke-root) och kör genom Dropbox synkronisering installationen.';
+$lang['dropbox_status_not_initialized'] = 'Avvaktan användarinitieringen';
+$lang['dropbox_user_invalid'] = 'Dropbox användare är ogiltig.';
+$lang['dropbox_user_no_access'] = 'Administratören har inte satt upp detta konto med Dropbox tillgång.';
+$lang['dropbox_users'] = 'Dropbox Användare';
