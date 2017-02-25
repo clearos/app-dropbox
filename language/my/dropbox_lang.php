20069b0e806dd7ab99561dd63a8cc3b16eb35b63
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:07:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/dropbox_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['dropbox_account_initialize_busy'] = 'Dropbox ကိုဝန်ဆောင်မှုအခြားအသုံးပြုသူစတင်လုပ်ဆောင်အလုပ်များဖြစ်ပါသည်။';
+$lang['dropbox_app_description'] = 'Dropbox ကို cloud storage, ဖိုင် synchronize လုပ်ပေးတဲ့နှင့် client software များကမ်းလှမ်းကြောင်း Dropbox ကို, Inc မှ operate ဖိုင်တစ်ဖိုင် hosting ဝန်ဆောင်မှုဖြစ်ပါတယ်။';
+$lang['dropbox_app_name'] = 'Dropbox ကို';
+$lang['dropbox_folder_size'] = 'Folder ကို Size ကို';
+$lang['dropbox_missing_home_folder'] = 'ပျောက်ဆုံးနေအိမ်မှာ folder ကို - န်ဆောင်မှု file မှအသုံးပြုသူ access ကိုစစ်ဆေးပါ။';
+$lang['dropbox_no_data'] = 'အဘယ်သူမျှမဒေတာ ...';
+$lang['dropbox_reset_complete'] = 'သင့် local Dropbox ကိုဒေသခံဖိုလ်ဒါကိုဖျက်ပစ်နှင့်ထပ်တူပြုခြင်းသတင်းအချက်အလက်မှပြန်လည်စတင်အကောင့်သိရသည်။';
+$lang['dropbox_reset_confirm'] = 'ဒါဟာသင့် local ကို Dropbox folder ကိုသငျသညျ (မဖျက်ပစ်) ပြန်လည်ပြင်ဆင်ခြင်းရှေ့တော်၌ထို Dropbox ကိုပေါ်တယ်မှဤစက်ကိရိယာ disassociate အကြံပြုသည်။';
+$lang['dropbox_reset_delete'] = 'Reset / Local Folder တခုကိုဖျက်ပစ်ပါ';
+$lang['dropbox_service_not_running'] = 'Dropbox ကိုဝန်ဆောင်မှုပြေးသည်မဟုတ်။';
+$lang['dropbox_starting_service'] = 'အနည်းဆုံးအသုံးပြုသူသူတို့၏အကောင့် configured သိရသည်သည်အထိသင်က Dropbox ကိုဝန်ဆောင်မှုကိုစတင်နိုင်လိမ့်မည်မဟုတ်ပေ။ အသုံးပြုသူတစ်ဦး (Non-အမြစ်) အဖြစ် Webconfig ဖို့ Login နှင့် Dropbox ကို syncronization setup ကိုတဆင့်ကို run ပါ။';
+$lang['dropbox_status_not_initialized'] = 'ဆိုင်းငံ့အသုံးပြုသူစတင်ခြင်း';
+$lang['dropbox_user_invalid'] = 'Dropbox ကိုအသုံးပြုသူမမှန်ကန်ဘူး။';
+$lang['dropbox_user_no_access'] = 'အဆိုပါအုပ်ချုပ်ရေးမှူး Dropbox ကိုဝင်ရောက်ခွင့်နှင့်အတူဤအကောင့်ဖွင့်မသတ်မှတ်ထားခဲ့သည်။';
+$lang['dropbox_users'] = 'Dropbox ကိုအသုံးပြုသူများသည်';
